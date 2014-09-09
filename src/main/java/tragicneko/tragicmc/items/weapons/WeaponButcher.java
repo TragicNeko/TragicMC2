7ee3577a7addd95634743cc727eb360bfd30a6ec
TragicNeko <TragicNeko@users.noreply.github.com>
2014-09-08 20:02:42 -0500

    Add knockback resistance buff to Butcher

--- src/main/java/tragicneko/tragicmc/items/weapons/WeaponButcher.java
+++ src/main/java/tragicneko/tragicmc/items/weapons/WeaponButcher.java
@@ -1,8 +1,12 @@
 package tragicneko.tragicmc.items.weapons;
 
+import java.util.UUID;
+
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.ai.attributes.AttributeModifier;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.potion.Potion;
@@ -41,4 +45,23 @@ public class WeaponButcher extends EpicWeapon {
 		}
 		return super.onLeftClickEntity(stack, player, entity);
 	} 
+	
+
+	@Override
+	public void onUpdate(ItemStack stack, World world, Entity entity, int numb, boolean flag)
+	{
+		if (world.isRemote || !(entity instanceof EntityPlayer) || !(TragicNewConfig.allowNonDoomsdayAbilities)) return;
+		
+		UUID uuidForMod = UUID.fromString("040d7d22-6b19-498b-8216-4316cf39387e");
+		AttributeModifier mod = new AttributeModifier(uuidForMod, "butcherModifier", 1.0, 0);
+		EntityPlayer player = (EntityPlayer) entity;
+		PropertyDoom doom = PropertyDoom.get(player);
+		
+		player.getEntityAttribute(SharedMonsterAttributes.knockbackResistance).removeModifier(mod);
+		
+		if (flag && doom != null && doom.getCurrentDoom() > 0)
+		{
+			player.getEntityAttribute(SharedMonsterAttributes.knockbackResistance).applyModifier(mod);
+		}
+	}
 }
