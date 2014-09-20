76649ba5f7c65f9c73b80bf7308a5a054aad1bb5
TragicNeko <TragicNeko@users.noreply.github.com>
2014-09-20 04:07:00 -0500

    Increase group size of Sirv on spawn

--- src/main/java/tragicneko/tragicmc/worldgen/biome/TragicBiome.java
+++ src/main/java/tragicneko/tragicmc/worldgen/biome/TragicBiome.java
@@ -39,7 +39,7 @@ public class TragicBiome extends BiomeGenBase {
 		if (TragicNewConfig.allowGreaterStin) this.spawnableMonsterList.add(new BiomeGenBase.SpawnListEntry(EntityStin.class, TragicNewConfig.greaterStinSC, 0, 1));
 		if (TragicNewConfig.allowBossNaturalSpawns && TragicNewConfig.allowTimeController) this.spawnableMonsterList.add(new BiomeGenBase.SpawnListEntry(EntityTimeController.class, TragicNewConfig.timeControllerSC, 0, 1));
 		if (TragicNewConfig.allowErkel) this.spawnableCreatureList.add(new BiomeGenBase.SpawnListEntry(EntityErkel.class, TragicNewConfig.erkelSC, 0, 3));
-		if (TragicNewConfig.allowSirv) this.spawnableMonsterList.add(new BiomeGenBase.SpawnListEntry(EntitySirv.class, TragicNewConfig.sirvSC, 3, 5));
+		if (TragicNewConfig.allowSirv) this.spawnableMonsterList.add(new BiomeGenBase.SpawnListEntry(EntitySirv.class, TragicNewConfig.sirvSC, 4, 6));
 		if (TragicNewConfig.allowWisp) this.spawnableCreatureList.add(new BiomeGenBase.SpawnListEntry(EntityWisp.class, TragicNewConfig.wispSC, 1, 3));
 	}
 	
