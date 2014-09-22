89ac16a8166fbb7ab7e65bb90aa7e38c9a99e861
TragicNeko <TragicNeko@users.noreply.github.com>
2014-09-22 00:11:58 -0500

    Update version

--- src/main/java/tragicneko/tragicmc/TragicMC.java
+++ src/main/java/tragicneko/tragicmc/TragicMC.java
@@ -102,7 +102,7 @@ public class TragicMC
 {	
 	public static final String MODNAME = "TragicMC 2";
 	public static final String MODID = "TragicMC";
-	public static final String VERSION = "1.33.1243 Beta";
+	public static final String VERSION = "1.36.1520 Beta";
 
 	@Instance(TragicMC.MODID)
 	public static TragicMC instance;
