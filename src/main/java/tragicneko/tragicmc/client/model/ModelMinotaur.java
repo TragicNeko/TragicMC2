72de941694617cd51490715942fee3b6ed30fed3
TragicNeko <TragicNeko@users.noreply.github.com>
2014-09-26 02:21:39 -0500

    Update Minotaur AI and model, also add animations

--- src/main/java/tragicneko/tragicmc/client/model/ModelMinotaur.java
+++ src/main/java/tragicneko/tragicmc/client/model/ModelMinotaur.java
@@ -3,162 +3,168 @@ package tragicneko.tragicmc.client.model;
 import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.model.ModelRenderer;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import tragicneko.tragicmc.entity.mob.EntityMinotaur;
 
 public class ModelMinotaur extends ModelBase
 {
-  //fields
-    ModelRenderer head;
-    ModelRenderer lefthorn;
-    ModelRenderer righthorn;
-    ModelRenderer nose;
-    ModelRenderer body;
-    ModelRenderer abdomen;
-    ModelRenderer rightarm;
-    ModelRenderer rightarmlower;
-    ModelRenderer leftarm;
-    ModelRenderer leftarmlower;
-    ModelRenderer rightleg;
-    ModelRenderer rightthigh;
-    ModelRenderer leftleg;
-    ModelRenderer leftthigh;
-    ModelRenderer righthoof;
-    ModelRenderer lefthoof;
-  
-  public ModelMinotaur()
-  {
-    textureWidth = 64;
-    textureHeight = 64;
-    
-      head = new ModelRenderer(this, 0, 0);
-      head.addBox(-3.5F, -8F, -6F, 7, 6, 8);
-      head.setRotationPoint(0F, -5F, 0F);
-      head.setTextureSize(64, 64);
-      head.mirror = true;
-      setRotation(head, 0F, 0F, 0F);
-      lefthorn = new ModelRenderer(this, 30, 0);
-      lefthorn.addBox(3.5F, -2F, 0F, 2, 2, 9);
-      lefthorn.setRotationPoint(0F, -11F, 0F);
-      lefthorn.setTextureSize(64, 64);
-      lefthorn.mirror = true;
-      setRotation(lefthorn, 0.2974289F, 0.3866576F, 0F);
-      righthorn = new ModelRenderer(this, 30, 0);
-      righthorn.addBox(-5.5F, -2F, 0F, 2, 2, 9);
-      righthorn.setRotationPoint(0F, -11F, 0F);
-      righthorn.setTextureSize(64, 64);
-      righthorn.mirror = true;
-      setRotation(righthorn, 0.2974289F, -0.3866576F, 0F);
-      nose = new ModelRenderer(this, 0, 25);
-      nose.addBox(-2F, 0F, -4F, 4, 3, 3);
-      nose.setRotationPoint(0F, -10F, -5F);
-      nose.setTextureSize(64, 64);
-      nose.mirror = true;
-      setRotation(nose, 0F, 0F, 0F);
-      body = new ModelRenderer(this, 16, 16);
-      body.addBox(-6F, 0F, -3F, 12, 9, 10);
-      body.setRotationPoint(0F, -7F, -1F);
-      body.setTextureSize(64, 64);
-      body.mirror = true;
-      setRotation(body, 0F, 0F, 0F);
-      abdomen = new ModelRenderer(this, 26, 36);
-      abdomen.addBox(-4F, 0F, 0F, 8, 6, 4);
-      abdomen.setRotationPoint(0F, 2F, 0F);
-      abdomen.setTextureSize(64, 64);
-      abdomen.mirror = true;
-      setRotation(abdomen, 0F, 0F, 0F);
-      rightarm = new ModelRenderer(this, 0, 35);
-      rightarm.addBox(-3F, -2F, -2F, 4, 12, 4);
-      rightarm.setRotationPoint(-7F, -5F, 0F);
-      rightarm.setTextureSize(64, 64);
-      rightarm.mirror = true;
-      setRotation(rightarm, -0.1189716F, 0F, 0.2082002F);
-      rightarmlower = new ModelRenderer(this, 0, 52);
-      rightarmlower.addBox(-6F, 8F, -2F, 3, 7, 3);
-      rightarmlower.setRotationPoint(-7F, -5F, 0F);
-      rightarmlower.setTextureSize(64, 64);
-      rightarmlower.mirror = true;
-      setRotation(rightarmlower, -0.1189716F, 0F, -0.2082002F);
-      leftarm = new ModelRenderer(this, 0, 35);
-      leftarm.addBox(-1F, -2F, -2F, 4, 12, 4);
-      leftarm.setRotationPoint(7F, -5F, 0F);
-      leftarm.setTextureSize(64, 64);
-      leftarm.mirror = true;
-      setRotation(leftarm, -0.1189716F, 0F, -0.2082002F);
-      leftarmlower = new ModelRenderer(this, 0, 52);
-      leftarmlower.addBox(3F, 8F, -2F, 3, 7, 3);
-      leftarmlower.setRotationPoint(7F, -5F, 0F);
-      leftarmlower.setTextureSize(64, 64);
-      leftarmlower.mirror = true;
-      setRotation(leftarmlower, -0.1189716F, 0F, 0.2082002F);
-      rightleg = new ModelRenderer(this, 0, 16);
-      rightleg.addBox(-1F, 8F, -2F, 3, 5, 3);
-      rightleg.setRotationPoint(-3F, 6F, 1F);
-      rightleg.setTextureSize(64, 64);
-      rightleg.mirror = true;
-      setRotation(rightleg, 0F, 0F, 0F);
-      rightthigh = new ModelRenderer(this, 16, 48);
-      rightthigh.addBox(-4F, 0F, -3F, 6, 8, 7);
-      rightthigh.setRotationPoint(-3F, 6F, 1F);
-      rightthigh.setTextureSize(64, 64);
-      rightthigh.mirror = true;
-      setRotation(rightthigh, 0F, 0F, 0F);
-      leftleg = new ModelRenderer(this, 0, 16);
-      leftleg.addBox(-2F, 8F, -2F, 3, 5, 3);
-      leftleg.setRotationPoint(3F, 6F, 1F);
-      leftleg.setTextureSize(64, 64);
-      leftleg.mirror = true;
-      setRotation(leftleg, 0F, 0F, 0F);
-      leftthigh = new ModelRenderer(this, 16, 48);
-      leftthigh.addBox(-2F, 0F, -3F, 6, 8, 7);
-      leftthigh.setRotationPoint(3F, 6F, 1F);
-      leftthigh.setTextureSize(64, 64);
-      leftthigh.mirror = true;
-      setRotation(leftthigh, 0F, 0F, 0F);
-      righthoof = new ModelRenderer(this, 42, 47);
-      righthoof.addBox(-2.5F, 13F, -2.5F, 5, 5, 5);
-      righthoof.setRotationPoint(-3F, 6F, 1F);
-      righthoof.setTextureSize(64, 64);
-      righthoof.mirror = true;
-      setRotation(righthoof, 0F, 0F, 0F);
-      lefthoof = new ModelRenderer(this, 42, 47);
-      lefthoof.addBox(-2.5F, 13F, -2.5F, 5, 5, 5);
-      lefthoof.setRotationPoint(3F, 6F, 1F);
-      lefthoof.setTextureSize(64, 64);
-      lefthoof.mirror = true;
-      setRotation(lefthoof, 0F, 0F, 0F);
-  }
-  
-  public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5)
-  {
-    super.render(entity, f, f1, f2, f3, f4, f5);
-    setRotationAngles(f, f1, f2, f3, f4, f5, entity);
-    head.render(f5);
-    lefthorn.render(f5);
-    righthorn.render(f5);
-    nose.render(f5);
-    body.render(f5);
-    abdomen.render(f5);
-    rightarm.render(f5);
-    rightarmlower.render(f5);
-    leftarm.render(f5);
-    leftarmlower.render(f5);
-    rightleg.render(f5);
-    rightthigh.render(f5);
-    leftleg.render(f5);
-    leftthigh.render(f5);
-    righthoof.render(f5);
-    lefthoof.render(f5);
-  }
-  
-  private void setRotation(ModelRenderer model, float x, float y, float z)
-  {
-    model.rotateAngleX = x;
-    model.rotateAngleY = y;
-    model.rotateAngleZ = z;
-  }
-  
-  public void setRotationAngles(float f, float f1, float f2, float f3, float f4, float f5, Entity entity)
-  {
-    super.setRotationAngles(f, f1, f2, f3, f4, f5, entity);
-  }
+	private ModelRenderer head;
+	private ModelRenderer body;
+	private ModelRenderer rightArm;
+	private ModelRenderer leftArm;
+	private ModelRenderer rightLeg;
+	private ModelRenderer leftLeg;
+
+	public ModelMinotaur()
+	{
+		textureWidth = 64;
+		textureHeight = 64;
+
+		//Head, Horns and Nose
+		head = new ModelRenderer(this, 0, 0);
+		head.addBox(-3.5F, -8F, -6F, 7, 6, 8);
+		head.setRotationPoint(0F, -5F, 0F);
+		ModelRenderer leftHorn = new ModelRenderer(this, 30, 0);
+		leftHorn.addBox(3.5F, -8F, 0F, 2, 2, 9);
+		leftHorn.rotateAngleX = 0.2974289F;
+		leftHorn.rotateAngleY = 0.3866576F;
+		head.addChild(leftHorn);
+		ModelRenderer rightHorn = new ModelRenderer(this, 30, 0);
+		rightHorn.addBox(-5.5F, -8F, 0F, 2, 2, 9);
+		rightHorn.rotateAngleX = 0.2974289F;
+		rightHorn.rotateAngleY = -0.3866576F;
+		head.addChild(rightHorn);
+		ModelRenderer nose = new ModelRenderer(this, 0, 25);
+		nose.addBox(-2F, -5F, -9F, 4, 3, 3);
+		head.addChild(nose);
+
+		//Body and Abdomen
+		body = new ModelRenderer(this, 16, 16);
+		body.addBox(-6F, 0F, -3F, 12, 9, 10);
+		body.setRotationPoint(0F, -7F, -1F);
+		ModelRenderer abdomen = new ModelRenderer(this, 26, 36);
+		abdomen.addBox(-4F, 8F, 0F, 8, 6, 4);
+		body.addChild(abdomen);
+
+		//Right Arm and Forearm
+		rightArm = new ModelRenderer(this, 0, 35);
+		rightArm.addBox(-3F, -2F, -2F, 4, 12, 4);
+		rightArm.setRotationPoint(-7F, -5F, 0F);
+		rightArm.rotateAngleX = -0.1189716F;
+		rightArm.rotateAngleZ = 0.2082002F;
+		ModelRenderer rightArm2 = new ModelRenderer(this, 0, 52);
+		rightArm2.addBox(-4F, 9F, -1F, 3, 7, 3);
+		rightArm2.rotateAngleX = -0.1189716F;
+		rightArm2.rotateAngleZ = -0.2082002F;
+		rightArm.addChild(rightArm2);
+
+		//Left Arm and Forearm
+		leftArm = new ModelRenderer(this, 0, 35);
+		leftArm.addBox(-1F, -2F, -2F, 4, 12, 4);
+		leftArm.setRotationPoint(7F, -5F, 0F);
+		leftArm.rotateAngleX = -0.1189716F;
+		leftArm.rotateAngleZ = -0.2082002F;
+		ModelRenderer leftArm2 = new ModelRenderer(this, 0, 52);
+		leftArm2.addBox(1F, 9F, -1F, 3, 7, 3);
+		leftArm2.rotateAngleX = -0.1189716F;
+		leftArm2.rotateAngleZ = 0.2082002F;
+		leftArm.addChild(leftArm2);
+
+		//Right Leg, Thigh and Hoof
+		rightLeg = new ModelRenderer(this, 0, 16);
+		rightLeg.addBox(-1F, 8F, -2F, 3, 5, 3);
+		rightLeg.setRotationPoint(-3F, 6F, 1F);
+		ModelRenderer rightThigh = new ModelRenderer(this, 16, 48);
+		rightThigh.addBox(-4F, 0F, -3F, 6, 8, 7);
+		rightLeg.addChild(rightThigh);
+		ModelRenderer rightHoof = new ModelRenderer(this, 42, 47);
+		rightHoof.addBox(-2.5F, 13F, -2.5F, 5, 5, 5);
+		rightLeg.addChild(rightHoof);
+
+		//Left Leg, Thigh and Hoof
+		leftLeg = new ModelRenderer(this, 0, 16);
+		leftLeg.addBox(-2F, 8F, -2F, 3, 5, 3);
+		leftLeg.setRotationPoint(3F, 6F, 1F);
+		ModelRenderer leftThigh = new ModelRenderer(this, 16, 48);
+		leftThigh.addBox(-3F, 0F, -3F, 6, 8, 7);
+		leftLeg.addChild(leftThigh);
+		ModelRenderer leftHoof = new ModelRenderer(this, 42, 47);
+		leftHoof.addBox(-2.5F, 13F, -2.5F, 5, 5, 5);
+		leftLeg.addChild(leftHoof);
+	}
+
+	public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5)
+	{
+		super.render(entity, f, f1, f2, f3, f4, f5);
+		this.setRotationAngles(f, f1, f2, f3, f4, f5, entity);
+		head.render(f5);
+		body.render(f5);
+		rightArm.render(f5);
+		leftArm.render(f5);
+		rightLeg.render(f5);
+		leftLeg.render(f5);
+	}
+
+	public void setRotationAngles(float par1, float par2, float par3, float par4, float par5, float par6, Entity entity)
+	{
+		this.head.rotateAngleY = par4 / (180F / (float)Math.PI);
+		this.head.rotateAngleX = par5 / (180F / (float)Math.PI);
+		this.leftLeg.rotateAngleX = -1.5F * this.simplifyAngle(par1, 13.0F) * par2;
+		this.rightLeg.rotateAngleX = 1.5F * this.simplifyAngle(par1, 13.0F) * par2;
+		this.leftArm.rotateAngleX = 0.55F * this.simplifyAngle(par1, 13.0F) * par2;
+		this.rightArm.rotateAngleX = -0.55F * this.simplifyAngle(par1, 13.0F) * par2; 
+	}
+
+	public void setLivingAnimations(EntityLivingBase entity, float par2, float par3, float par4)
+	{
+		if (!(entity instanceof EntityMinotaur)) return; //This is here to prevent problems from happening with the Claymation, temporarily
+		
+		EntityMinotaur mino = (EntityMinotaur)entity;
+
+		if (mino.isCharging())
+		{
+			body.rotateAngleX = 0.4082002F;
+			body.offsetY = 0.235F;
+			head.offsetY = 0.265F;
+			head.offsetZ = -0.095F;
+			head.rotateAngleX = 0.4082002F;
+			head.rotateAngleZ = 0.0F;
+			leftArm.rotateAngleX = -0.8189716F;
+			leftArm.rotateAngleZ = 0.1082002F;
+			rightArm.rotateAngleX = -0.8189716F;
+			rightArm.rotateAngleZ = -0.1082002F;
+			leftArm.offsetY = 0.245F;
+			rightArm.offsetY = 0.245F;
+			leftLeg.rotateAngleX = -0.8189716F;
+			rightLeg.rotateAngleX = 0.8189716F;
+			rightLeg.offsetY = 0.145F;
+			rightLeg.offsetZ = 0.025F;
+			leftLeg.offsetY = 0.145F;
+			leftLeg.offsetZ = -0.025F;
+		}
+		else
+		{
+			head.offsetY = 0.0F;
+			head.offsetZ = 0.0F;
+			body.rotateAngleX = 0.0F;
+			body.offsetY = 0.0F;
+			leftArm.offsetY = 0.0F;
+			rightArm.offsetY = 0.0F;
+			rightLeg.offsetY = 0.0F;
+			leftLeg.offsetY = 0.0F;
+			rightLeg.offsetZ = 0.0F;
+			leftLeg.offsetZ = 0.0F;
+			leftArm.rotateAngleX = -0.1189716F;
+			leftArm.rotateAngleZ = -0.2082002F;
+			rightArm.rotateAngleX = -0.1189716F;
+			rightArm.rotateAngleZ = 0.2082002F;
+		}
+
+	}
+
+	private float simplifyAngle(float par1, float par2)
+	{
+		return (Math.abs(par1 % par2 - par2 * 0.5F) - par2 * 0.25F) / (par2 * 0.25F);
+	}
 
 }
--- src/main/java/tragicneko/tragicmc/entity/mob/EntityMinotaur.java
+++ src/main/java/tragicneko/tragicmc/entity/mob/EntityMinotaur.java
@@ -27,21 +27,51 @@ public class EntityMinotaur extends TragicMob {
 
 	public EntityMinotaur(World par1World) {
 		super(par1World);
-		this.setSize(0.7F, 2.5F);
+		this.setSize(0.725F, 2.575F);
 		this.stepHeight = 1.0F;
 		this.experienceValue = 8;
-		this.getNavigator().setAvoidsWater(true);
 		this.tasks.addTask(0, new EntityAISwimming(this));
-		this.tasks.addTask(2, new EntityAIAttackOnCollide(this, EntityLivingBase.class, 1.0D, true));
+		this.tasks.addTask(0, new EntityAIAttackOnCollide(this, EntityLivingBase.class, 1.0D, true));
 		this.tasks.addTask(7, new EntityAILookIdle(this));
 		this.tasks.addTask(6, new EntityAIWander(this, 0.75D));
 		this.tasks.addTask(8, new EntityAIWatchClosest(this, EntityLivingBase.class, 32.0F));
-		this.tasks.addTask(3, new EntityAIMoveTowardsTarget(this, 0.65D, 32.0F));
+		this.tasks.addTask(1, new EntityAIMoveTowardsTarget(this, 0.65D, 32.0F));
 		this.targetTasks.addTask(2, new EntityAIHurtByTarget(this, true));
-		this.targetTasks.addTask(3, new EntityAINearestAttackableTarget(this, EntityPlayer.class, 0, true)); /*
-		this.canCorrupt = false;
-		this.isCorruptible = false;
-		this.isChangeable = false; */
+		this.targetTasks.addTask(3, new EntityAINearestAttackableTarget(this, EntityPlayer.class, 0, true));
+	}
+
+	@Override
+	protected void entityInit()
+	{
+		super.entityInit();
+		this.dataWatcher.addObject(16, Integer.valueOf(0));
+	}
+
+	public int getChargeTicks()
+	{
+		return this.dataWatcher.getWatchableObjectInt(16);
+	}
+
+	private void setChargeTicks(int i)
+	{
+		this.dataWatcher.updateObject(16, i);
+	}
+
+	private void decrementChargeTicks()
+	{
+		int pow = this.getChargeTicks();
+		this.setChargeTicks(--pow);
+	}
+
+	public boolean isCharging()
+	{
+		return this.getChargeTicks() > 0;
+	}
+
+	@Override
+	public boolean canCorrupt()
+	{
+		return false;
 	}
 
 	public EnumCreatureAttribute getCreatureAttribute()
@@ -60,7 +90,7 @@ public class EntityMinotaur extends TragicMob {
 		this.getEntityAttribute(SharedMonsterAttributes.maxHealth).setBaseValue(42.0);
 		this.getEntityAttribute(SharedMonsterAttributes.movementSpeed).setBaseValue(.35);
 		this.getEntityAttribute(SharedMonsterAttributes.attackDamage).setBaseValue(8.0);
-		this.getEntityAttribute(SharedMonsterAttributes.followRange).setBaseValue(32);
+		this.getEntityAttribute(SharedMonsterAttributes.followRange).setBaseValue(32.0);
 		this.getEntityAttribute(SharedMonsterAttributes.knockbackResistance).setBaseValue(0.5);
 	}
 
@@ -68,29 +98,45 @@ public class EntityMinotaur extends TragicMob {
 	{
 		super.onLivingUpdate();
 
-		if (this.worldObj.isDaytime() && this.worldObj.getLightBrightness((int)this.posX, (int)this.posY, (int)this.posZ) > 0.4F)
+		if (this.worldObj.isRemote) return;
+
+		if (this.isCharging())
 		{
-			if (this.ticksExisted % 120 == 0 && !this.worldObj.isRemote && this.getHealth() < this.getMaxHealth())
+			this.decrementChargeTicks();
+			this.setSprinting(true);
+		}
+		else
+		{
+			this.setSprinting(false);
+		}
+
+		if (this.worldObj.isDaytime())
+		{
+			if (this.ticksExisted % 60 == 0 && this.getHealth() < this.getMaxHealth())
 			{
 				this.heal(1.0F);
 			}
 		}
 
-		if (this.getAttackTarget() != null && this.getDistanceToEntity(this.getAttackTarget()) > 1.0F && this.getDistanceToEntity(this.getAttackTarget()) < 8.0F 
-				&& this.onGround && rand.nextInt(60) == 0)
+		if (this.getAttackTarget() != null && this.getDistanceToEntity(this.getAttackTarget()) > 1.0F && this.getDistanceToEntity(this.getAttackTarget()) <= 8.0F 
+				&& this.onGround && rand.nextInt(16) == 0 && this.onGround && !this.isCharging() && this.canEntityBeSeen(this.getAttackTarget()))
 		{
 			double d0 = this.getAttackTarget().posX - this.posX;
 			double d1 = this.getAttackTarget().posZ - this.posZ;
-			float f2 = MathHelper.sqrt_double(d0 * d0 + d1 * d1);
-			this.motionX = d0 / (double)f2 * 2.5D * 0.800000011920929D + this.motionX * 0.60000000298023224D;
-			this.motionZ = d1 / (double)f2 * 2.5D * 0.800000011920929D + this.motionZ * 0.60000000298023224D;
-			this.motionY = 0.05;
+			double d2 = this.getAttackTarget().posY - this.posY;
+			float f2 = MathHelper.sqrt_double(d0 * d0 + d1 * d1 + d2 * d2);
+			this.motionX = d0 / (double)f2 * 2.5D * 0.700000011920929D + this.motionX * 0.40000000298023224D;
+			this.motionZ = d1 / (double)f2 * 2.5D * 0.700000011920929D + this.motionZ * 0.40000000298023224D;
+			this.motionY = d1 / (double)f2 * 1.1D * 0.200000011920929D + this.motionZ * 0.20000000298023224D;
+			this.setChargeTicks(20);
 		}
 	}
 
 	public boolean attackEntityFrom(DamageSource par1DamageSource, float par2)
 	{ 
-		if (par1DamageSource.getEntity() != null && par1DamageSource.getEntity() instanceof EntityPlayer && !this.worldObj.isRemote)
+		if (this.worldObj.isRemote) return false;
+
+		if (par1DamageSource.getEntity() != null && par1DamageSource.getEntity() instanceof EntityPlayer && !par1DamageSource.isProjectile() && !par1DamageSource.isMagicDamage())
 		{
 			EntityPlayer player = (EntityPlayer) par1DamageSource.getEntity();
 
@@ -103,21 +149,21 @@ public class EntityMinotaur extends TragicMob {
 			}
 			else
 			{
-				player.attackEntityFrom(DamageSource.causeMobDamage(this), 1.0F);
+				player.attackEntityFrom(DamageSource.causeMobDamage(this), 0.5F);
 			}
 		}
 
-		if (!par1DamageSource.isProjectile() && !par1DamageSource.isExplosion())
-		{
-			par2 *= 0.65F;
-		}
+		if (!par1DamageSource.isProjectile() && !par1DamageSource.isExplosion() && !par1DamageSource.isMagicDamage()) par2 *= 0.65F;
+		if (this.isCharging()) par2 *= 0.45F;
 
 		return super.attackEntityFrom(par1DamageSource, par2);
 	}
 
 	public boolean attackEntityAsMob(Entity par1Entity)
 	{
-		if (super.attackEntityAsMob(par1Entity))
+		boolean result = super.attackEntityAsMob(par1Entity);
+		
+		if (result)
 		{
 			if (par1Entity instanceof EntityLivingBase && rand.nextInt(8) == 0)
 			{
@@ -136,14 +182,17 @@ public class EntityMinotaur extends TragicMob {
 				}
 			}
 
-			par1Entity.motionX *= 1.2000000059604645D;
-			par1Entity.motionZ *= 1.2D;
-			par1Entity.motionY += 0.3D;
-
+			if (this.isCharging())
+			{
+				par1Entity.motionX *= 1.2000000059604645D;
+				par1Entity.motionZ *= 1.2D;
+				par1Entity.motionY += 0.3D;
+			}
 		}
-		return super.attackEntityAsMob(par1Entity);
+		
+		return result;
 	}
-	
+
 	public int getTotalArmorValue()
 	{
 		return 6;
