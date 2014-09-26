ab74a3364ade240e23e00c7bb33b7f574227261d
TragicNeko <TragicNeko@users.noreply.github.com>
2014-09-26 18:27:46 -0500

    Rewrite Tragic Neko's AI, update model and add animations

--- src/main/java/tragicneko/tragicmc/client/model/ModelTragicNeko.java
+++ src/main/java/tragicneko/tragicmc/client/model/ModelTragicNeko.java
@@ -1,131 +1,176 @@
 package tragicneko.tragicmc.client.model;
 
+import tragicneko.tragicmc.entity.mob.EntityTragicNeko;
 import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.model.ModelRenderer;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
 
 public class ModelTragicNeko extends ModelBase
 {
-	ModelRenderer Head;
-	ModelRenderer LeftEar;
-	ModelRenderer RightEar;
-	ModelRenderer Body;
-	ModelRenderer Tail;
-	ModelRenderer TailTip;
-	ModelRenderer RightArm;
-	ModelRenderer LeftArm;
-	ModelRenderer RightLeg;
-	ModelRenderer LeftLeg;
-	ModelRenderer RocketLauncherShaft;
-	ModelRenderer RocketLauncherBody;
+	private ModelRenderer head;
+	private ModelRenderer leftEar;
+	private ModelRenderer rightEar;
+	private ModelRenderer body;
+	private ModelRenderer tail;
+	private ModelRenderer rightArm;
+	private ModelRenderer leftArm;
+	private ModelRenderer rightLeg;
+	private ModelRenderer leftLeg;
+	private ModelRenderer rocketLauncher;
 
 	public ModelTragicNeko()
 	{
 		textureWidth = 64;
 		textureHeight = 64;
 
-		Head = new ModelRenderer(this, 0, 0);
-		Head.addBox(-3F, -6F, -4F, 6, 6, 6);
-		Head.setRotationPoint(0F, 0F, 0F);
-		Head.setTextureSize(64, 32);
-		Head.mirror = true;
-		setRotation(Head, 0F, 0F, 0F);
-		LeftEar = new ModelRenderer(this, 9, 38);
-		LeftEar.addBox(0F, -5F, -4F, 3, 3, 1);
-		LeftEar.setRotationPoint(0F, -3F, 0F);
-		LeftEar.setTextureSize(64, 32);
-		LeftEar.mirror = true;
-		setRotation(LeftEar, -0.3346075F, 0F, 0.4461433F);
-		RightEar = new ModelRenderer(this, 0, 38);
-		RightEar.addBox(-3F, -5F, -4F, 3, 3, 1);
-		RightEar.setRotationPoint(0F, -3F, 0F);
-		RightEar.setTextureSize(64, 32);
-		RightEar.mirror = true;
-		setRotation(RightEar, -0.3346075F, 0F, -0.4461433F);
-		Body = new ModelRenderer(this, 16, 16);
-		Body.addBox(-3F, 0F, -2F, 6, 12, 3);
-		Body.setRotationPoint(0F, 0F, 0F);
-		Body.setTextureSize(64, 32);
-		Body.mirror = true;
-		setRotation(Body, 0F, 0F, 0F);
-		Tail = new ModelRenderer(this, 40, 0);
-		Tail.addBox(0F, -8F, 0F, 2, 8, 2);
-		Tail.setRotationPoint(0F, 12F, 1F);
-		Tail.setTextureSize(64, 32);
-		Tail.mirror = true;
-		setRotation(Tail, -0.7063936F, 0F, 0F);
-		TailTip = new ModelRenderer(this, 40, 0);
-		TailTip.addBox(0F, -15F, 2.5F, 2, 8, 2);
-		TailTip.setRotationPoint(0F, 12F, 1F);
-		TailTip.setTextureSize(64, 32);
-		TailTip.mirror = true;
-		setRotation(TailTip, -0.3717861F, 0F, 0F);
-		RightArm = new ModelRenderer(this, 40, 16);
-		RightArm.addBox(-2F, -2F, -2F, 2, 10, 2);
-		RightArm.setRotationPoint(-3F, 2F, 0F);
-		RightArm.setTextureSize(64, 32);
-		RightArm.mirror = true;
-		setRotation(RightArm, -0.2974289F, 0F, 0.4461433F);
-		LeftArm = new ModelRenderer(this, 40, 16);
-		LeftArm.addBox(-1F, -1F, -2F, 2, 10, 2);
-		LeftArm.setRotationPoint(3F, 2F, 0F);
-		LeftArm.setTextureSize(64, 32);
-		LeftArm.mirror = true;
-		setRotation(LeftArm, -0.4833219F, 0F, 0.4461433F);
-		RightLeg = new ModelRenderer(this, 0, 16);
-		RightLeg.addBox(-1.5F, 0F, -2F, 2, 12, 2);
-		RightLeg.setRotationPoint(-1F, 12F, 0F);
-		RightLeg.setTextureSize(64, 32);
-		RightLeg.mirror = true;
-		setRotation(RightLeg, 0F, 0F, 0F);
-		LeftLeg = new ModelRenderer(this, 0, 16);
-		LeftLeg.addBox(-1.5F, 0F, -2F, 2, 12, 2);
-		LeftLeg.setRotationPoint(2F, 12F, 0F);
-		LeftLeg.setTextureSize(64, 32);
-		LeftLeg.mirror = true;
-		setRotation(LeftLeg, 0F, 0F, 0F);
-		RocketLauncherShaft = new ModelRenderer(this, 24, 30);
-		RocketLauncherShaft.addBox(-2F, 0F, -9F, 5, 4, 15);
-		RocketLauncherShaft.setRotationPoint(-5F, 9F, -4F);
-		RocketLauncherShaft.setTextureSize(64, 64);
-		RocketLauncherShaft.mirror = true;
-		setRotation(RocketLauncherShaft, 0F, 0F, 0F);
-		RocketLauncherBody = new ModelRenderer(this, 0, 44);
-		RocketLauncherBody.addBox(-3F, -1F, -2F, 7, 6, 7);
-		RocketLauncherBody.setRotationPoint(-5F, 9F, -4F);
-		RocketLauncherBody.setTextureSize(64, 64);
-		RocketLauncherBody.mirror = true;
-		setRotation(RocketLauncherBody, 0F, 0F, 0F);
+		//Head and Ears
+		head = new ModelRenderer(this, 0, 0);
+		head.addBox(-3F, -6F, -4F, 6, 6, 6);
+		head.setRotationPoint(0F, 0F, 0F);
+		leftEar = new ModelRenderer(this, 9, 38);
+		leftEar.addBox(-1F, -7F, -4F, 3, 3, 1);
+		leftEar.rotateAngleX = -0.3346075F;
+		leftEar.rotateAngleZ = 0.4461433F;
+		head.addChild(leftEar);
+		rightEar = new ModelRenderer(this, 0, 38);
+		rightEar.addBox(-2.5F, -7.5F, -4F, 3, 3, 1);
+		rightEar.rotateAngleX = -0.3346075F;
+		rightEar.rotateAngleZ = -0.4461433F;
+		head.addChild(rightEar);
+
+		//Body and Tail
+		body = new ModelRenderer(this, 16, 16);
+		body.addBox(-3F, 0F, -2F, 6, 12, 3);
+		body.setRotationPoint(0F, 0F, 0F);
+
+		tail = new ModelRenderer(this, 40, 0);
+		tail.addBox(0F, -3F, 5F, 2, 8, 2);
+		tail.setRotationPoint(0F, 0F, 3F);
+		tail.rotateAngleX = -0.7063936F;
+		ModelRenderer tailTip = new ModelRenderer(this, 40, 0);
+		tailTip.addBox(0F, -8F, 5.5F, 2, 8, 2);
+		tailTip.rotateAngleX = 0.3717861F;
+		tail.addChild(tailTip);
+
+		//Right Arm
+		rightArm = new ModelRenderer(this, 40, 16);
+		rightArm.addBox(-2F, -2F, -2F, 2, 10, 2);
+		rightArm.setRotationPoint(-3F, 2F, 0F);
+		rightArm.rotateAngleX = -0.2974289F;
+		rightArm.rotateAngleZ = 0.4461433F;
+		rocketLauncher = new ModelRenderer(this, 24, 30);
+		rocketLauncher.addBox(-2F, 6F, -7F, 5, 4, 15);
+		rightArm.addChild(rocketLauncher);
+		ModelRenderer rocketLauncherBody = new ModelRenderer(this, 0, 44);
+		rocketLauncherBody.addBox(-3F, 5F, -2F, 7, 6, 7);
+		rocketLauncher.addChild(rocketLauncherBody);
+
+		//Left Arm
+		leftArm = new ModelRenderer(this, 40, 16);
+		leftArm.addBox(-1F, -1F, -2F, 2, 10, 2);
+		leftArm.setRotationPoint(3F, 2F, 0F);
+		leftArm.rotateAngleX = -0.4833219F;
+		leftArm.rotateAngleZ = 0.4461433F;
+
+		//Right Leg
+		rightLeg = new ModelRenderer(this, 0, 16);
+		rightLeg.addBox(-1.5F, 0F, -2F, 2, 12, 2);
+		rightLeg.setRotationPoint(-1F, 12F, 0F);
+
+		//Left Leg
+		leftLeg = new ModelRenderer(this, 0, 16);
+		leftLeg.addBox(-1.5F, 0F, -2F, 2, 12, 2);
+		leftLeg.setRotationPoint(2F, 12F, 0F);
 	}
 
 	public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5)
 	{
 		super.render(entity, f, f1, f2, f3, f4, f5);
 		setRotationAngles(f, f1, f2, f3, f4, f5, entity);
-		Head.render(f5);
-		LeftEar.render(f5);
-		RightEar.render(f5);
-		Body.render(f5);
-		Tail.render(f5);
-		TailTip.render(f5);
-		RightArm.render(f5);
-		LeftArm.render(f5);
-		RightLeg.render(f5);
-		LeftLeg.render(f5);
-		RocketLauncherShaft.render(f5);
-		RocketLauncherBody.render(f5);
-	}
-
-	private void setRotation(ModelRenderer model, float x, float y, float z)
-	{
-		model.rotateAngleX = x;
-		model.rotateAngleY = y;
-		model.rotateAngleZ = z;
+		head.render(f5);
+		body.render(f5);
+		tail.render(f5);
+		rightArm.render(f5);
+		leftArm.render(f5);
+		rightLeg.render(f5);
+		leftLeg.render(f5);
 	}
 
 	public void setRotationAngles(float f, float f1, float f2, float f3, float f4, float f5, Entity entity)
 	{
 		super.setRotationAngles(f, f1, f2, f3, f4, f5, entity);
+
+		this.head.rotateAngleY = f3 / (180F / (float)Math.PI);
+		this.head.rotateAngleX = f4 / (180F / (float)Math.PI);
+
+		this.leftLeg.rotateAngleX = -1.85F * this.simplifyAngle(entity.ticksExisted, 15.0F) * f1;
+		this.rightLeg.rotateAngleX = 1.85F * this.simplifyAngle(entity.ticksExisted, 15.0F) * f1;
 	}
 
+	public void setLivingAnimations(EntityLivingBase entity, float par2, float par3, float par4)
+	{
+		EntityTragicNeko neko = (EntityTragicNeko) entity;
+
+		if (neko.deathTime > 0)
+		{
+			this.rocketLauncher.offsetY = 0.635F;
+		}
+		else
+		{
+			this.rocketLauncher.offsetY = 0.0F;
+			
+			this.tail.offsetY = 0.215F;
+			this.tail.offsetZ = -0.205F;
+			this.tail.offsetX = -0.0275F + 0.019875F * this.simplifyAngle(entity.ticksExisted, 30.0F);
+
+			this.tail.rotateAngleY = 0.275F * this.simplifyAngle(entity.ticksExisted, 15.0F);
+			this.tail.rotateAngleZ =  0.01875F * this.simplifyAngle(entity.ticksExisted, 15.0F);
+			this.tail.rotateAngleX = -0.7063936F;
+
+			if (neko.getFlickTime() > 0)
+			{
+				this.rightEar.rotateAngleZ = -0.115F + 0.01875F * this.simplifyAngle(entity.ticksExisted, 5.0F);
+				this.leftEar.rotateAngleZ = 0.115F + -0.01875F * this.simplifyAngle(entity.ticksExisted, 5.0F);
+				this.rightEar.offsetY = 0.0175F;
+				this.leftEar.offsetX = 0.075F;
+				this.rightEar.offsetX = -0.075F;
+			}
+			else
+			{
+				this.rightEar.rotateAngleZ = -0.4461433F;
+				this.leftEar.rotateAngleZ = 0.4461433F;
+				this.rightEar.offsetX = 0.0F;
+				this.rightEar.offsetY = 0.0F;
+				this.leftEar.offsetX = 0.0F;
+
+				if (neko.getThrowingTicks() > 0)
+				{
+					this.leftArm.rotateAngleX = -0.45F + -1.65F * this.simplifyAngle(neko.getThrowingTicks(), 30.0F);
+					this.leftArm.rotateAngleZ = -0.45F + 0.35F * this.simplifyAngle(neko.getThrowingTicks(), 30.0F);
+				}
+				else
+				{
+					leftArm.rotateAngleX = -0.4833219F;
+					leftArm.rotateAngleZ = 0.4461433F;
+
+					if (neko.getAttackTime() > 0)
+					{
+						this.rightArm.rotateAngleX = 0.45F + -1.65F * this.simplifyAngle(neko.getThrowingTicks(), 30.0F);
+						this.rightArm.rotateAngleZ = 0.45F + 0.35F * this.simplifyAngle(neko.getThrowingTicks(), 30.0F);
+					}
+					else
+					{
+						rightArm.rotateAngleX = -0.4833219F;
+						rightArm.rotateAngleZ = 0.4461433F;
+					}
+				}
+			}
+		}
+	}
+
+	private float simplifyAngle(float par1, float par2)
+	{
+		return (Math.abs(par1 % par2 - par2 * 0.5F) - par2 * 0.25F) / (par2 * 0.25F);
+	}
 }
--- src/main/java/tragicneko/tragicmc/entity/mob/EntityTragicNeko.java
+++ src/main/java/tragicneko/tragicmc/entity/mob/EntityTragicNeko.java
@@ -18,12 +18,15 @@ import net.minecraft.entity.ai.EntityAIWander;
 import net.minecraft.entity.ai.EntityAIWatchClosest;
 import net.minecraft.entity.ai.attributes.AttributeModifier;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.projectile.EntityThrowable;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.MathHelper;
 import net.minecraft.world.World;
+import tragicneko.tragicmc.TragicMC;
 import tragicneko.tragicmc.entity.projectile.EntityNekoClusterBomb;
 import tragicneko.tragicmc.entity.projectile.EntityNekoMiniBomb;
 import tragicneko.tragicmc.entity.projectile.EntityNekoRocket;
@@ -32,26 +35,113 @@ import tragicneko.tragicmc.main.TragicItems;
 
 public class EntityTragicNeko extends TragicMob {
 
-	private int projCooldown;
-	private boolean isAboutToFire;
-	private int firingTicks;
-
 	public EntityTragicNeko(World par1World) {
 		super(par1World);
 		this.setSize(0.4F, 1.955F);
 		this.experienceValue = 16;
 		this.tasks.addTask(0, new EntityAISwimming(this));
-		this.tasks.addTask(2, new EntityAIAttackOnCollide(this, EntityLivingBase.class, 1.0D, true));
+		this.tasks.addTask(0, new EntityAIAttackOnCollide(this, EntityLivingBase.class, 1.0D, true));
 		this.tasks.addTask(7, new EntityAILookIdle(this));
-		this.tasks.addTask(6, new EntityAIWander(this, 0.55D));
-		this.tasks.addTask(1, new EntityAIWatchClosest(this, EntityLivingBase.class, 32.0F));
-		this.tasks.addTask(3, new EntityAIMoveTowardsTarget(this, 1.0D, 32.0F));
+		this.tasks.addTask(6, new EntityAIWander(this, 0.65D));
+		this.tasks.addTask(8, new EntityAIWatchClosest(this, EntityLivingBase.class, 32.0F));
+		this.tasks.addTask(1, new EntityAIMoveTowardsTarget(this, 0.85D, 32.0F));
 		this.targetTasks.addTask(2, new EntityAIHurtByTarget(this, true));
-		this.targetTasks.addTask(3, new EntityAINearestAttackableTarget(this, EntityPlayer.class, 0, true)); /*
-		this.canCorrupt = false;
-		this.isCorruptible = false;
-		this.isChangeable = false; */
-		this.isAboutToFire = false;
+		this.targetTasks.addTask(3, new EntityAINearestAttackableTarget(this, EntityPlayer.class, 0, true));
+	}
+
+	@Override
+	public boolean canCorrupt()
+	{
+		return false;
+	}
+
+	@Override
+	protected void entityInit()
+	{
+		super.entityInit();
+		this.dataWatcher.addObject(16, Integer.valueOf(0));
+		this.dataWatcher.addObject(17, Integer.valueOf(0));
+		this.dataWatcher.addObject(18, Integer.valueOf(0));
+		this.dataWatcher.addObject(19, Integer.valueOf(0));
+	}
+
+	public int getFiringTicks()
+	{
+		return this.dataWatcher.getWatchableObjectInt(16);
+	}
+
+	private void setFiringTicks(int i)
+	{
+		this.dataWatcher.updateObject(16, i);
+	}
+
+	private void incrementFiringTicks()
+	{
+		int pow = this.getFiringTicks();
+		this.setFiringTicks(++pow);
+	}
+
+	public boolean isAboutToFire()
+	{
+		return this.getFiringTicks() > 0 && this.getFiringTicks() <= 60;
+	}
+
+	public boolean hasFired()
+	{
+		return this.getFiringTicks() > 60;
+	}
+
+	public boolean canFire()
+	{
+		return this.getFiringTicks() >= 120;
+	}
+
+	public int getThrowingTicks()
+	{
+		return this.dataWatcher.getWatchableObjectInt(17);
+	}
+
+	private void setThrowingTicks(int i)
+	{
+		this.dataWatcher.updateObject(17, i);
+	}
+
+	private void decrementThrowingTicks()
+	{
+		int pow = this.getThrowingTicks();
+		this.setThrowingTicks(--pow);
+	}
+
+	public int getAttackTime()
+	{
+		return this.dataWatcher.getWatchableObjectInt(18);
+	}
+
+	private void setAttackTime(int i)
+	{
+		this.dataWatcher.updateObject(18, i);
+	}
+
+	private void decrementAttackTime()
+	{
+		int pow = this.getAttackTime();
+		this.setAttackTime(--pow);
+	}
+
+	public int getFlickTime()
+	{
+		return this.dataWatcher.getWatchableObjectInt(19);
+	}
+
+	private void setFlickTime(int i)
+	{
+		this.dataWatcher.updateObject(19, i);
+	}
+
+	private void decrementFlickTime()
+	{
+		int pow = this.getFlickTime();
+		this.setFlickTime(--pow);
 	}
 
 	public boolean isAIEnabled()
@@ -63,9 +153,9 @@ public class EntityTragicNeko extends TragicMob {
 	{
 		super.applyEntityAttributes();
 		this.getEntityAttribute(SharedMonsterAttributes.maxHealth).setBaseValue(80.0);
-		this.getEntityAttribute(SharedMonsterAttributes.movementSpeed).setBaseValue(.33);
-		this.getEntityAttribute(SharedMonsterAttributes.attackDamage).setBaseValue(2.0);
-		this.getEntityAttribute(SharedMonsterAttributes.followRange).setBaseValue(64.0);
+		this.getEntityAttribute(SharedMonsterAttributes.movementSpeed).setBaseValue(.335);
+		this.getEntityAttribute(SharedMonsterAttributes.attackDamage).setBaseValue(6.0);
+		this.getEntityAttribute(SharedMonsterAttributes.followRange).setBaseValue(32.0);
 	}
 
 	public int getMaxSpawnedInChunk()
@@ -84,59 +174,61 @@ public class EntityTragicNeko extends TragicMob {
 	{
 		super.onLivingUpdate();
 
-		if (!this.worldObj.isRemote)
-		{
-			if (this.projCooldown > 0)
-			{
-				projCooldown--;
-			}
+		if (this.worldObj.isRemote) return;
 
-			if (this.firingTicks > 0)
-			{
-				this.firingTicks--;
-			}
+		if (this.getAttackTarget() != null) this.incrementFiringTicks();
+		if (this.getThrowingTicks() > 0) this.decrementThrowingTicks();
+		if (this.getAttackTime() > 0) this.decrementAttackTime();
+		if (this.getFlickTime() > 0) this.decrementFlickTime();
+
+		int i = this.getHealth() <= this.getMaxHealth() / 2 ? 4 : 16;
+
+		if (this.getAttackTarget() != null && rand.nextInt(i) == 0 && this.canFire() && this.ticksExisted % 20 == 0 && this.getThrowingTicks() == 0 && this.getAttackTime() == 0
+				&& this.getDistanceToEntity(this.getAttackTarget()) >= 2.0F)
+		{
+			this.setFiringTicks(0);
 		}
 
-		if (this.getAttackTarget() != null && rand.nextInt(8) == 0 && !this.worldObj.isRemote && !this.isAboutToFire)
+		if (this.getAttackTarget() != null)
 		{
-			if (this.projCooldown <= 0)
-			{
-				this.isAboutToFire = true;
-			}
-		}
-
-		if (!this.worldObj.isRemote && this.isAboutToFire && this.getAttackTarget() != null)
-		{
-			this.isAboutToFire = false;
+			if (this.getFlickTime() > 0) this.setFlickTime(0);
 			
-			this.firingTicks = 60;
-			
-			this.doProjectileAttack(this.getAttackTarget(), this.getDistanceToEntity(this.getAttackTarget()));
-			this.projCooldown = 51 + rand.nextInt(20);
-		}
-		
-		UUID modUUID = UUID.fromString("ef7bc471-3df8-4d0d-8aa6-8f52ae0a6045");
-		AttributeModifier mod = new AttributeModifier(modUUID, "tragicNekoSpeedDebuff", -0.50, 0);
-		
-		if (this.firingTicks > 0)
-		{
-			this.getEntityAttribute(SharedMonsterAttributes.movementSpeed).removeModifier(mod);
-			this.getEntityAttribute(SharedMonsterAttributes.movementSpeed).applyModifier(mod);
+			if (this.getFiringTicks() == 60)
+			{
+				this.doMissleAttack();
+			}
+			else if (this.hasFired() && rand.nextInt(8) == 0 && this.getFiringTicks() % 20 == 0 && this.getAttackTime() == 0)
+			{
+				this.throwRandomProjectile();
+				this.setThrowingTicks(20);
+			}
 		}
 		else
 		{
-			this.getEntityAttribute(SharedMonsterAttributes.movementSpeed).removeModifier(mod);
+			if (this.ticksExisted % 20 == 0 && rand.nextInt(4) == 0) this.setFlickTime(10);
+			this.setFiringTicks(65);
 		}
+		
+		AttributeModifier mod = new AttributeModifier(UUID.fromString("ef7bc471-3df8-4d0d-8aa6-8f52ae0a6045"), "tragicNekoSpeedDebuff", -0.50, 0);
+		this.getEntityAttribute(SharedMonsterAttributes.movementSpeed).removeModifier(mod);
+
+		if (this.isAboutToFire() || this.getThrowingTicks() > 0)
+		{
+			this.getEntityAttribute(SharedMonsterAttributes.movementSpeed).applyModifier(mod);
+		}
+	}
+	
+	public void onDeath(DamageSource par1DamageSource)
+	{
+		super.onDeath(par1DamageSource);
+		if (!this.worldObj.isRemote) this.setThrowingTicks(20);
 	}
 
 	public void onDeathUpdate()
 	{
 		super.onDeathUpdate();
-		
-		if (this.worldObj.isRemote)
-		{
-			return;
-		}
+
+		if (this.worldObj.isRemote) return;
 
 		if (this.deathTime % 10 == 0)
 		{
@@ -181,39 +273,44 @@ public class EntityTragicNeko extends TragicMob {
 		}
 	}
 
-	private void doProjectileAttack(EntityLivingBase entity, float distance) 
+	private void doMissleAttack()
 	{
-		Entity theProjectile = null;
-
+		EntityLivingBase entity = this.getAttackTarget();
 		double d0 = entity.posX - this.posX;
 		double d1 = entity.boundingBox.minY + (double)(this.height / 2.0F) - (this.posY + (double)(this.height / 2.0F));
 		double d2 = entity.posZ - this.posZ;
+		EntityNekoRocket rocket = new EntityNekoRocket(this.worldObj, this, d0, d1, d2);
+		rocket.shootingEntity = this;
+		rocket.posY = this.posY + (0.5D);
+		rocket.posX = this.posX - (Math.sin(this.rotationYaw * (float)Math.PI / 180.0F) * 0.025D);
+		rocket.posZ = this.posZ - (Math.cos(this.rotationYaw * (float)Math.PI / 180.0F) * 0.025D);
+		this.worldObj.spawnEntityInWorld(rocket);
+	}
+
+	private void throwRandomProjectile() 
+	{
+		EntityThrowable theProjectile = null;
 
 		switch (rand.nextInt(5))
 		{
-		case 0:
-			theProjectile = new EntityNekoRocket(this.worldObj, this, d0, d1, d2);
-			break;
 		case 1:
-			theProjectile = new EntityNekoStickyBomb(this.worldObj, d0, d1, d2);
+			theProjectile = new EntityNekoStickyBomb(this.worldObj, this);
 			break;
 		case 2:
-			theProjectile = new EntityNekoMiniBomb(this.worldObj, d0, d1, d2);
-			break;
-		case 3:
-			theProjectile = new EntityNekoRocket(this.worldObj, this, d0, d1, d2);
+			theProjectile = new EntityNekoClusterBomb(this.worldObj, this);
 			break;
 		case 4:
-			theProjectile = new EntityNekoStickyBomb(this.worldObj, d0, d1, d2);
+			theProjectile = new EntityNekoStickyBomb(this.worldObj, this);
+			break;
+		default:
+			theProjectile = new EntityNekoMiniBomb(this.worldObj, this);
 			break;
 		}
-
-		if (theProjectile != null)
-		{
-			theProjectile.posY = this.posY + (this.height * 2 / 3);
-			this.worldObj.spawnEntityInWorld(theProjectile);
-		}
-
+		
+		TragicMC.logInfo("Should've thrown a projectile");
+		theProjectile.motionX = (this.getAttackTarget().posX - this.posX) * 0.335D;
+		theProjectile.motionZ = (this.getAttackTarget().posZ - this.posZ) * 0.335D;
+		this.worldObj.spawnEntityInWorld(theProjectile);
 	}
 
 	public boolean getCanSpawnHere()
@@ -250,14 +347,41 @@ public class EntityTragicNeko extends TragicMob {
 
 	public boolean attackEntityFrom(DamageSource par1DamageSource, float par2)
 	{ 
-		if (par1DamageSource.isExplosion())
-		{
-			return false;
-		}
-
-		Boolean result = super.attackEntityFrom(par1DamageSource, par2);
+		if (par1DamageSource.isExplosion() || this.worldObj.isRemote) return false;
+		
+		if (this.getFiringTicks() < 60) this.setFiringTicks(61);
+		boolean result = super.attackEntityFrom(par1DamageSource, par2);
+		if (result) this.setAttackTime(10);
 
 		return result;
+	}
+
+	public boolean attackEntityAsMob(Entity par1Entity)
+	{
+		if (this.getThrowingTicks() > 0) return false;
+		if (this.getFiringTicks() < 60) this.setFiringTicks(61);
+		boolean result = super.attackEntityAsMob(par1Entity);
+		if (result) this.setAttackTime(10);
+		return result;
+	}
+	
+	@Override
+	public void readEntityFromNBT(NBTTagCompound tag) {
+		super.readEntityFromNBT(tag);
+		if (tag.hasKey("attackTime")) this.setAttackTime(tag.getInteger("attackTime"));
+		if (tag.hasKey("firingTicks")) this.setFiringTicks(tag.getInteger("firingTicks"));
+		if (tag.hasKey("throwingTicks")) this.setThrowingTicks(tag.getInteger("throwingTicks"));
+		if (tag.hasKey("flickTime")) this.setFlickTime(tag.getInteger("flickTime"));
+	}
+
+	@Override
+	public void writeEntityToNBT(NBTTagCompound tag)
+	{
+		super.writeEntityToNBT(tag);
+		tag.setInteger("attackTime", this.getAttackTime());
+		tag.setInteger("firingTicks", this.getFiringTicks());
+		tag.setInteger("throwingTicks", this.getThrowingTicks());
+		tag.setInteger("flickTime", this.getFlickTime());
 	}
 
 	@Override
