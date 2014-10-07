f21a0227c0afeed2b1c1eb7d8c1dab0258f96e1d
TragicNeko <TragicNeko@users.noreply.github.com>
2014-10-07 07:57:22 -0500

    Updated Erkel model, added basic animations

--- src/main/java/tragicneko/tragicmc/client/model/ModelErkel.java
+++ src/main/java/tragicneko/tragicmc/client/model/ModelErkel.java
@@ -6,159 +6,95 @@ import net.minecraft.entity.Entity;
 
 public class ModelErkel extends ModelBase
 {
-  //fields
-    ModelRenderer Shape1;
-    ModelRenderer Shape2;
-    ModelRenderer Shape3;
-    ModelRenderer Shape4;
-    ModelRenderer Shape5;
-    ModelRenderer Shape6;
-    ModelRenderer Shape7;
-    ModelRenderer Shape8;
-    ModelRenderer Shape9;
-    ModelRenderer Shape10;
-    ModelRenderer Shape11;
-    ModelRenderer Shape12;
-    ModelRenderer Shape13;
-    ModelRenderer Shape14;
-    ModelRenderer Shape15;
-    ModelRenderer Shape16;
-  
-  public ModelErkel()
-  {
-    textureWidth = 64;
-    textureHeight = 32;
-    
-      Shape1 = new ModelRenderer(this, 0, 24);
-      Shape1.addBox(-2F, -1F, -2F, 4, 2, 4);
-      Shape1.setRotationPoint(0F, 11F, 0F);
-      Shape1.setTextureSize(64, 32);
-      Shape1.mirror = true;
-      setRotation(Shape1, 0F, 0F, 0F);
-      Shape2 = new ModelRenderer(this, 0, 14);
-      Shape2.addBox(-3F, 0F, -3F, 6, 2, 6);
-      Shape2.setRotationPoint(0F, 12F, 0F);
-      Shape2.setTextureSize(64, 32);
-      Shape2.mirror = true;
-      setRotation(Shape2, 0F, 0F, 0F);
-      Shape3 = new ModelRenderer(this, 0, 4);
-      Shape3.addBox(-4F, 1F, -3F, 1, 2, 6);
-      Shape3.setRotationPoint(0F, 13F, 0F);
-      Shape3.setTextureSize(64, 32);
-      Shape3.mirror = true;
-      setRotation(Shape3, 0F, 0F, 0F);
-      Shape4 = new ModelRenderer(this, 0, 4);
-      Shape4.addBox(3F, 1F, -3F, 1, 2, 6);
-      Shape4.setRotationPoint(0F, 13F, 0F);
-      Shape4.setTextureSize(64, 32);
-      Shape4.mirror = true;
-      setRotation(Shape4, 0F, 0F, 0F);
-      Shape5 = new ModelRenderer(this, 0, 0);
-      Shape5.addBox(-3F, 1F, -4F, 6, 2, 1);
-      Shape5.setRotationPoint(0F, 13F, 0F);
-      Shape5.setTextureSize(64, 32);
-      Shape5.mirror = true;
-      setRotation(Shape5, 0F, 0F, 0F);
-      Shape6 = new ModelRenderer(this, 0, 0);
-      Shape6.addBox(-3F, 0F, 3F, 6, 2, 1);
-      Shape6.setRotationPoint(0F, 14F, 0F);
-      Shape6.setTextureSize(64, 32);
-      Shape6.mirror = true;
-      setRotation(Shape6, 0F, 0F, 0F);
-      Shape7 = new ModelRenderer(this, 16, 0);
-      Shape7.addBox(-1.5F, 0F, -1.5F, 3, 6, 3);
-      Shape7.setRotationPoint(0F, 14F, 0F);
-      Shape7.setTextureSize(64, 32);
-      Shape7.mirror = true;
-      setRotation(Shape7, 0F, 0F, 0F);
-      Shape8 = new ModelRenderer(this, 30, 0);
-      Shape8.addBox(-3F, 2F, -3F, 6, 3, 6);
-      Shape8.setRotationPoint(0F, 18F, 0F);
-      Shape8.setTextureSize(64, 32);
-      Shape8.mirror = true;
-      setRotation(Shape8, 0F, 0F, 0F);
-      Shape9 = new ModelRenderer(this, 20, 12);
-      Shape9.addBox(-1F, 0F, 0F, 1, 1, 1);
-      Shape9.setRotationPoint(-1F, 23F, 0F);
-      Shape9.setTextureSize(64, 32);
-      Shape9.mirror = true;
-      setRotation(Shape9, 0F, 0F, 0F);
-      Shape10 = new ModelRenderer(this, 20, 12);
-      Shape10.addBox(0F, 0F, 0F, 1, 1, 1);
-      Shape10.setRotationPoint(1F, 23F, 0F);
-      Shape10.setTextureSize(64, 32);
-      Shape10.mirror = true;
-      setRotation(Shape10, 0F, 0F, 0F);
-      Shape11 = new ModelRenderer(this, 26, 14);
-      Shape11.addBox(0F, -2F, 0F, 1, 2, 1);
-      Shape11.setRotationPoint(2F, 12F, -2F);
-      Shape11.setTextureSize(64, 32);
-      Shape11.mirror = true;
-      setRotation(Shape11, 0F, 0F, 0.1858931F);
-      Shape12 = new ModelRenderer(this, 32, 14);
-      Shape12.addBox(0F, -3F, -1F, 3, 1, 3);
-      Shape12.setRotationPoint(1F, 12F, -2F);
-      Shape12.setTextureSize(64, 32);
-      Shape12.mirror = true;
-      setRotation(Shape12, 0F, 0F, 0.1858931F);
-      Shape13 = new ModelRenderer(this, 26, 14);
-      Shape13.addBox(0F, -2F, 0F, 1, 2, 1);
-      Shape13.setRotationPoint(-3F, 14F, 2F);
-      Shape13.setTextureSize(64, 32);
-      Shape13.mirror = true;
-      setRotation(Shape13, -0.2230717F, 0F, -0.1487144F);
-      Shape14 = new ModelRenderer(this, 32, 14);
-      Shape14.addBox(-1F, -3F, -1F, 3, 1, 3);
-      Shape14.setRotationPoint(-3F, 14F, 2F);
-      Shape14.setTextureSize(64, 32);
-      Shape14.mirror = true;
-      setRotation(Shape14, -0.2230717F, 0F, -0.1487144F);
-      Shape15 = new ModelRenderer(this, 26, 14);
-      Shape15.addBox(0F, -2F, 0F, 1, 2, 1);
-      Shape15.setRotationPoint(2F, 20F, 0F);
-      Shape15.setTextureSize(64, 32);
-      Shape15.mirror = true;
-      setRotation(Shape15, 0F, 0F, 0.8179294F);
-      Shape16 = new ModelRenderer(this, 32, 14);
-      Shape16.addBox(-1F, -3F, -1F, 3, 1, 3);
-      Shape16.setRotationPoint(2F, 20F, 0F);
-      Shape16.setTextureSize(64, 32);
-      Shape16.mirror = true;
-      setRotation(Shape16, 0F, 0F, 0.8179294F);
-  }
-  
-  public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5)
-  {
-    super.render(entity, f, f1, f2, f3, f4, f5);
-    setRotationAngles(f, f1, f2, f3, f4, f5, entity);
-    Shape1.render(f5);
-    Shape2.render(f5);
-    Shape3.render(f5);
-    Shape4.render(f5);
-    Shape5.render(f5);
-    Shape6.render(f5);
-    Shape7.render(f5);
-    Shape8.render(f5);
-    Shape9.render(f5);
-    Shape10.render(f5);
-    Shape11.render(f5);
-    Shape12.render(f5);
-    Shape13.render(f5);
-    Shape14.render(f5);
-    Shape15.render(f5);
-    Shape16.render(f5);
-  }
-  
-  private void setRotation(ModelRenderer model, float x, float y, float z)
-  {
-    model.rotateAngleX = x;
-    model.rotateAngleY = y;
-    model.rotateAngleZ = z;
-  }
-  
-  public void setRotationAngles(float f, float f1, float f2, float f3, float f4, float f5, Entity entity)
-  {
-    super.setRotationAngles(f, f1, f2, f3, f4, f5, entity);
-  }
+	private ModelRenderer body;
+	private ModelRenderer footRight;
+	private ModelRenderer footLeft;
+	private ModelRenderer sideShroom;
+	private ModelRenderer topShroom;
+	private ModelRenderer topShroom2;
 
+	public ModelErkel()
+	{
+		textureWidth = 64;
+		textureHeight = 32;
+
+		body = new ModelRenderer(this, 16, 0);
+		body.addBox(-1.5F, 0F, -1.5F, 3, 6, 3);
+		body.setRotationPoint(0F, 14F, 0F);
+		ModelRenderer bodyThickness = new ModelRenderer(this, 30, 0);
+		bodyThickness.addBox(-3F, 6F, -3F, 6, 3, 6);
+		body.addChild(bodyThickness);
+
+		ModelRenderer shroomPart = new ModelRenderer(this, 0, 24);
+		shroomPart.addBox(-2F, -4F, -2F, 4, 2, 4);
+		body.addChild(shroomPart);
+		ModelRenderer shroomPart2 = new ModelRenderer(this, 0, 14);
+		shroomPart2.addBox(-3F, -2F, -3F, 6, 2, 6);
+		body.addChild(shroomPart2);
+		ModelRenderer shroomPart3 = new ModelRenderer(this, 0, 4);
+		shroomPart3.addBox(-4F, 0F, -3F, 1, 2, 6);
+		body.addChild(shroomPart3);
+		ModelRenderer shroomPart4 = new ModelRenderer(this, 0, 4);
+		shroomPart4.addBox(3F, 0F, -3F, 1, 2, 6);
+		body.addChild(shroomPart4);
+		ModelRenderer shroomPart5 = new ModelRenderer(this, 0, 0);
+		shroomPart5.addBox(-3F, 0F, -4F, 6, 2, 1);
+		body.addChild(shroomPart5);
+		ModelRenderer shroomPart6 = new ModelRenderer(this, 0, 0);
+		shroomPart6.addBox(-3F, 0F, 3F, 6, 2, 1);
+		body.addChild(shroomPart6);
+		
+		footRight = new ModelRenderer(this, 20, 12);
+		footRight.addBox(-2F, 9F, 0F, 1, 1, 1);
+		body.addChild(footRight);
+		
+		footLeft = new ModelRenderer(this, 20, 12);
+		footLeft.addBox(1F, 9F, 0F, 1, 1, 1);
+		body.addChild(footLeft);
+		
+		topShroom = new ModelRenderer(this, 26, 14);
+		topShroom.addBox(2F, -4F, -2F, 1, 2, 1);
+		topShroom.rotateAngleX = 0.1858931F;
+		body.addChild(topShroom);
+		ModelRenderer topShroomCap = new ModelRenderer(this, 32, 14);
+		topShroomCap.addBox(1F, -5F, -3F, 3, 1, 3);
+		topShroom.addChild(topShroomCap);
+		
+		topShroom2 = new ModelRenderer(this, 26, 14);
+		topShroom2.addBox(-3F, -4F, 2F, 1, 2, 1);
+		topShroom2.rotateAngleX = -0.2230717F;
+		topShroom2.rotateAngleZ = -0.1487144F;
+		body.addChild(topShroom2);
+		ModelRenderer topShroom2Cap = new ModelRenderer(this, 32, 14);
+		topShroom2Cap.addBox(-4F, -5F, 1F, 3, 1, 3);
+		topShroom2.addChild(topShroom2Cap);
+		
+		sideShroom = new ModelRenderer(this, 26, 14);
+		sideShroom.addBox(4F, 1F, 0F, 1, 2, 1);
+		sideShroom.rotateAngleZ = 0.817929F;
+		body.addChild(sideShroom);
+		ModelRenderer sideShroom2 = new ModelRenderer(this, 32, 14);
+		sideShroom2.addBox(3F, 0F, -1F, 3, 1, 3);
+		sideShroom.addChild(sideShroom2);
+	}
+
+	public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5)
+	{
+		super.render(entity, f, f1, f2, f3, f4, f5);
+		setRotationAngles(f, f1, f2, f3, f4, f5, entity);
+		body.render(f5);
+	}
+	
+	public void setRotationAngles(float f, float f1, float f2, float f3, float f4, float f5, Entity entity)
+	{		
+		body.rotateAngleY = f3 / (180F / (float)Math.PI);
+		body.rotateAngleX = f4 / (180F / (float)Math.PI);
+		footLeft.rotateAngleX = -0.25F * this.simplifyAngle(f, 5.0F) * f1;
+		footRight.rotateAngleX = 0.25F * this.simplifyAngle(f, 5.0F) * f1;
+	}
+
+	private float simplifyAngle(float par1, float par2)
+	{
+		return (Math.abs(par1 % par2 - par2 * 0.5F) - par2 * 0.25F) / (par2 * 0.25F);
+	}
 }
