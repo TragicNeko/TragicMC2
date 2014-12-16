a21b77aee4e27c694ed639ed6576d7bac22ca7ba
TragicNeko <TragicNeko@users.noreply.github.com>
2014-12-16 07:04:15 -0600

    Start work on adding weapon/armor lores to the new system

--- src/main/java/tragicneko/tragicmc/util/LoreHelper.java
+++ src/main/java/tragicneko/tragicmc/util/LoreHelper.java
@@ -13,11 +13,40 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumChatFormatting;
 import net.minecraft.util.MathHelper;
 import net.minecraft.util.WeightedRandom;
-import scala.actors.threadpool.Arrays;
 import tragicneko.tragicmc.TragicEnchantments;
 import tragicneko.tragicmc.TragicMC;
+import tragicneko.tragicmc.items.armor.ArmorDark;
+import tragicneko.tragicmc.items.armor.ArmorHunter;
+import tragicneko.tragicmc.items.armor.ArmorLight;
+import tragicneko.tragicmc.items.armor.ArmorMercury;
+import tragicneko.tragicmc.items.armor.ArmorSkull;
+import tragicneko.tragicmc.items.armor.ArmorTungsten;
+import tragicneko.tragicmc.items.armor.TragicArmor;
+import tragicneko.tragicmc.items.weapons.EpicWeapon;
+import tragicneko.tragicmc.items.weapons.ItemJack;
+import tragicneko.tragicmc.items.weapons.ItemScythe;
+import tragicneko.tragicmc.items.weapons.TragicWeapon;
 import tragicneko.tragicmc.items.weapons.WeaponBeastlyClaws;
 import tragicneko.tragicmc.items.weapons.WeaponBlindingLight;
+import tragicneko.tragicmc.items.weapons.WeaponButcher;
+import tragicneko.tragicmc.items.weapons.WeaponCelestialAegis;
+import tragicneko.tragicmc.items.weapons.WeaponCelestialLongbow;
+import tragicneko.tragicmc.items.weapons.WeaponDragonFang;
+import tragicneko.tragicmc.items.weapons.WeaponEnigmaShield;
+import tragicneko.tragicmc.items.weapons.WeaponFrozenLightning;
+import tragicneko.tragicmc.items.weapons.WeaponGravitySpike;
+import tragicneko.tragicmc.items.weapons.WeaponGuiltyThorn;
+import tragicneko.tragicmc.items.weapons.WeaponHarmonyBell;
+import tragicneko.tragicmc.items.weapons.WeaponHuntersBow;
+import tragicneko.tragicmc.items.weapons.WeaponNekoLauncher;
+import tragicneko.tragicmc.items.weapons.WeaponParanoia;
+import tragicneko.tragicmc.items.weapons.WeaponPitchBlack;
+import tragicneko.tragicmc.items.weapons.WeaponReaperScythe;
+import tragicneko.tragicmc.items.weapons.WeaponSplinter;
+import tragicneko.tragicmc.items.weapons.WeaponThardus;
+import tragicneko.tragicmc.items.weapons.WeaponTitan;
+import tragicneko.tragicmc.items.weapons.WeaponTungstenJack;
+import tragicneko.tragicmc.items.weapons.WeaponWitheringAxe;
 
 public class LoreHelper {
 
@@ -25,10 +54,28 @@ public class LoreHelper {
 
 	static
 	{
-
 		//Armor
+		addToLoreMap(TragicArmor.class, new Lore[] {}, new EnchantEntry[][] {{}, {}, {}, {}});
+		addToLoreMap(ArmorDark.class, new Lore[] {}, new EnchantEntry[][] {{}, {}, {}, {}});
+		addToLoreMap(ArmorHunter.class, new Lore[] {}, new EnchantEntry[][] {{}, {}, {}, {}});
+		addToLoreMap(ArmorLight.class, new Lore[] {}, new EnchantEntry[][] {{}, {}, {}, {}});
+		addToLoreMap(ArmorMercury.class, new Lore[] {}, new EnchantEntry[][] {{}, {}, {}, {}});
+		addToLoreMap(ArmorSkull.class, new Lore[] {}, new EnchantEntry[][] {{}, {}, {}, {}});
+		addToLoreMap(ArmorTungsten.class, new Lore[] {}, new EnchantEntry[][] {{}, {}, {}, {}});
+		
+		//Tools
+		addToLoreMap(ItemJack.class, new Lore[] {}, new EnchantEntry[][] {{}, {}, {}, {}});
+		addToLoreMap(WeaponTungstenJack.class, new Lore[] {}, new EnchantEntry[][] {{}, {}, {}, {}});
+		addToLoreMap(ItemScythe.class, new Lore[] {}, new EnchantEntry[][] {{}, {}, {}, {}});
 
 		//Weapons
+		//This entry works as a default for all of the weapons if they don't have any enchants set, as well it'll be used for the Mercury Dagger
+		addToLoreMap(TragicWeapon.class, new Lore[] {new Lore(25, "Boring.", 1), new Lore(15, "Nice.", 1), new Lore(5, "Interesting.", 1), new Lore(15, "Lame", 1), new Lore(25, "Ha.", 2),
+				new Lore(15, "Awesome.", 2), new Lore(10, "That's fascinating.", 2), new Lore(5, "That's nice.", 2), new Lore(25, "That's amazing!", 3), new Lore(15, "Fantastic!", 3),
+				new Lore(5, "I'm shuddering with excitement!", 3), new Lore(5, "Ama-zuh-zing!", 3)},
+			new EnchantEntry[][] {{}, {new EnchantEntry(Enchantment.unbreaking, 1)}, {new EnchantEntry(Enchantment.unbreaking, 2)},
+				{new EnchantEntry(Enchantment.unbreaking, 3), new EnchantEntry(Enchantment.sharpness, 1)}});
+		
 		addToLoreMap(WeaponBeastlyClaws.class, new Lore[] {new Lore(25, "That's beastly.", 1), new Lore(15, "Epic.", 1), new Lore(5, "Knockout!", 1),
 				new Lore(10, "Roar!", 1), new Lore(15, "Combo!", 1), new Lore(5, "Let's fight!", 1), new Lore(5, "Sucker punch!", 1),
 				new Lore(25, "Just getting started!", 2), new Lore(20, "Just sharpening my claws!", 2), new Lore(15, "One-two punch!", 2),
@@ -48,7 +95,29 @@ public class LoreHelper {
 			new EnchantEntry[][] {{}, {new EnchantEntry(TragicEnchantments.Absolve, 1)}, {new EnchantEntry(TragicEnchantments.Absolve, 3), new EnchantEntry(Enchantment.unbreaking, 1)},
 				{new EnchantEntry(TragicEnchantments.Absolve, 5), new EnchantEntry(Enchantment.unbreaking, 1), new EnchantEntry(Enchantment.fireAspect, 1)}});
 		
+		addToLoreMap(WeaponCelestialAegis.class, new Lore[] {}, new EnchantEntry[][] {{}, {}, {}, {}});
+		
+		addToLoreMap(WeaponCelestialLongbow.class, new Lore[] {}, new EnchantEntry[][] {{}, {}, {}, {}});
+		addToLoreMap(WeaponEnigmaShield.class, new Lore[] {}, new EnchantEntry[][] {{}, {}, {}, {}});
+		addToLoreMap(WeaponFrozenLightning.class, new Lore[] {}, new EnchantEntry[][] {{}, {}, {}, {}});
+		addToLoreMap(WeaponGravitySpike.class, new Lore[] {}, new EnchantEntry[][] {{}, {}, {}, {}});
+		addToLoreMap(WeaponGuiltyThorn.class, new Lore[] {}, new EnchantEntry[][] {{}, {}, {}, {}});
+		addToLoreMap(WeaponHarmonyBell.class, new Lore[] {}, new EnchantEntry[][] {{}, {}, {}, {}});
+		addToLoreMap(WeaponHuntersBow.class, new Lore[] {}, new EnchantEntry[][] {{}, {}, {}, {}});
+		addToLoreMap(WeaponNekoLauncher.class, new Lore[] {}, new EnchantEntry[][] {{}, {}, {}, {}});
+		addToLoreMap(WeaponPitchBlack.class, new Lore[] {}, new EnchantEntry[][] {{}, {}, {}, {}});
+		addToLoreMap(WeaponReaperScythe.class, new Lore[] {}, new EnchantEntry[][] {{}, {}, {}, {}});
+		addToLoreMap(WeaponWitheringAxe.class, new Lore[] {}, new EnchantEntry[][] {{}, {}, {}, {}});
+		
 		//Epic weapons
+		addToLoreMap(EpicWeapon.class, new Lore[] {}, new EnchantEntry[][] {{}, {}, {}, {}});
+		addToLoreMap(WeaponButcher.class, new Lore[] {}, new EnchantEntry[][] {{}, {}, {}, {}});
+		addToLoreMap(WeaponDragonFang.class, new Lore[] {}, new EnchantEntry[][] {{}, {}, {}, {}});
+		addToLoreMap(WeaponParanoia.class, new Lore[] {}, new EnchantEntry[][] {{}, {}, {}, {}});
+		addToLoreMap(WeaponSplinter.class, new Lore[] {}, new EnchantEntry[][] {{}, {}, {}, {}});
+		addToLoreMap(WeaponThardus.class, new Lore[] {}, new EnchantEntry[][] {{}, {}, {}, {}});
+		addToLoreMap(WeaponTitan.class, new Lore[] {}, new EnchantEntry[][] {{}, {}, {}, {}});
+		
 	}
 
 	public static void addToLoreMap(Class clazz, LoreEntry entry)
