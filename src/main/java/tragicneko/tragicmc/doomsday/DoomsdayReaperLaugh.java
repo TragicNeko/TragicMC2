a1fdbb9cb60bd0fbb777e6fe22507170fa3f918f
TragicNeko <TragicNeko@users.noreply.github.com>
2014-12-28 22:19:05 -0600

    Update Reaper Laugh to fire Pumpkinbombs

--- src/main/java/tragicneko/tragicmc/doomsday/DoomsdayReaperLaugh.java
+++ src/main/java/tragicneko/tragicmc/doomsday/DoomsdayReaperLaugh.java
@@ -1,7 +1,6 @@
 package tragicneko.tragicmc.doomsday;
 
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.entity.projectile.EntityWitherSkull;
 import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.EnumChatFormatting;
@@ -9,6 +8,7 @@ import net.minecraft.util.MathHelper;
 import tragicneko.tragicmc.TragicNewConfig;
 import tragicneko.tragicmc.TragicPotions;
 import tragicneko.tragicmc.doomsday.Doomsday.IExtendedDoomsday;
+import tragicneko.tragicmc.entity.projectile.EntityPumpkinbomb;
 import tragicneko.tragicmc.properties.PropertyDoom;
 
 public class DoomsdayReaperLaugh extends Doomsday implements IExtendedDoomsday {
@@ -33,17 +33,20 @@ public class DoomsdayReaperLaugh extends Doomsday implements IExtendedDoomsday {
 	}
 
 	@Override
-	public void useDoomsday(DoomsdayEffect effect, PropertyDoom doom,	EntityPlayer player, boolean crucMoment) 
+	public void useDoomsday(DoomsdayEffect effect, PropertyDoom doom, EntityPlayer player, boolean crucMoment) 
 	{		
-		for (int l = 0; l < 5; l++)
+		for (int l = 0; l < 3; l++)
 		{
 			double d0 = (MathHelper.getRandomIntegerInRange(rand, -4, 4) + player.posX) - player.posX; 
 			double d1 = (MathHelper.getRandomIntegerInRange(rand, -2, 2) + player.posY) - player.posY;
-			double d2 = (MathHelper.getRandomIntegerInRange(rand, -4, 4) + player.posZ) - player.posZ; 
+			double d2 = (MathHelper.getRandomIntegerInRange(rand, -4, 4) + player.posZ) - player.posZ;
 
-			EntityWitherSkull fireball = new EntityWitherSkull(player.worldObj, player, d0, d1, d2);
-			fireball.setPosition(player.posX + (d0 * 0.115), player.posY + 0.6D, player.posZ + (d2 * 0.115));
-			player.worldObj.spawnEntityInWorld(fireball);
+			EntityPumpkinbomb bomb = new EntityPumpkinbomb(player.worldObj, player);
+			bomb.setPosition(player.posX + (d0 * 0.115), player.posY + 0.6D, player.posZ + (d2 * 0.115));
+			bomb.motionX = d0;
+			bomb.motionY = d1;
+			bomb.motionZ = d2;
+			player.worldObj.spawnEntityInWorld(bomb);
 		}	
 	}
 	
