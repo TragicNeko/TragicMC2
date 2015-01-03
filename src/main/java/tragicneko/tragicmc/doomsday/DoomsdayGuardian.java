6a0275f1e7439e4eaa389ee74bedf8e5a6f1059d
TragicNeko <TragicNeko@users.noreply.github.com>
2015-01-03 17:36:09 -0600

    Add Crucial Moment for the Guardians Call Doomsday

--- src/main/java/tragicneko/tragicmc/doomsday/DoomsdayGuardian.java
+++ src/main/java/tragicneko/tragicmc/doomsday/DoomsdayGuardian.java
@@ -4,6 +4,7 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.EnumChatFormatting;
 import net.minecraft.util.MathHelper;
+import tragicneko.tragicmc.TragicMC;
 import tragicneko.tragicmc.entity.EntityGuardianShield;
 import tragicneko.tragicmc.properties.PropertyDoom;
 
@@ -15,28 +16,30 @@ public class DoomsdayGuardian extends Doomsday {
 
 	@Override
 	public void doInitialEffects(DoomsdayEffect effect, PropertyDoom doom, EntityPlayer player, boolean crucMoment) {
-		
+
 		player.addChatMessage(new ChatComponentText(EnumChatFormatting.RED + "You have used Guardian's Call!"));
 		if (crucMoment) player.addChatMessage(new ChatComponentText(EnumChatFormatting.GOLD + "Crucial Moment!"));
-		
-		float f = Math.max(1 / (this.getCrisis(player)), 20F); //Minimum health for the shield is 20, maximum varies depending on health system
+
+		float f = Math.max(1 / (this.getCrisis(player)) * 10F, 20F); //Minimum health for the shield is 20, maximum varies depending on health system, vanilla would allow a max of 200
 		double d0 = 0.0D; //placeholder value in order to complete construction
-		
+		if (crucMoment) player.heal(player.getHealth() / 2);
+		TragicMC.logInfo("Shield health is " + f);
+
 		EntityGuardianShield shield = new EntityGuardianShield(player.worldObj, player, d0, d0, d0).setShieldMaxHealth(f);
 		shield.setPosition(player.posX + 2.25, player.posY + 0.3, player.posZ);
 		shield.setShieldOffsets(2.25, 0);
 		player.worldObj.spawnEntityInWorld(shield);
-		
+
 		EntityGuardianShield shield2 = new EntityGuardianShield(player.worldObj, player, d0, d0, d0).setShieldMaxHealth(f);
 		shield2.setPosition(player.posX - 2.25, player.posY + 0.3, player.posZ);
 		shield2.setShieldOffsets(-2.25, 0);
 		player.worldObj.spawnEntityInWorld(shield2);
-		
+
 		EntityGuardianShield shield3 = new EntityGuardianShield(player.worldObj, player, d0, d0, d0).setShieldMaxHealth(f);
 		shield3.setPosition(player.posX, player.posY + 0.3, player.posZ + 2.25);
 		shield3.setShieldOffsets(0, 2.25);
 		player.worldObj.spawnEntityInWorld(shield3);
-		
+
 		EntityGuardianShield shield4 = new EntityGuardianShield(player.worldObj, player, d0, d0, d0).setShieldMaxHealth(f);
 		shield4.setPosition(player.posX, player.posY + 0.3, player.posZ - 2.25);
 		shield4.setShieldOffsets(0, -2.25);
