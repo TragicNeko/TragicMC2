dedb4e18360e8a2a393bd83cd2fbe7824588c02d
TragicNeko <TragicNeko@users.noreply.github.com>
2015-01-18 08:28:11 -0600

    Add Overlord spawn to Synapse

--- src/main/java/tragicneko/tragicmc/worldgen/biome/BiomeDecoratorSynapse.java
+++ src/main/java/tragicneko/tragicmc/worldgen/biome/BiomeDecoratorSynapse.java
@@ -1,9 +1,9 @@
 package tragicneko.tragicmc.worldgen.biome;
 
-import net.minecraft.entity.boss.EntityDragon;
 import net.minecraft.world.biome.BiomeDecorator;
 import net.minecraft.world.biome.BiomeGenBase;
 import tragicneko.tragicmc.dimension.SynapseWorldProvider;
+import tragicneko.tragicmc.entity.boss.EntityOverlordCore;
 
 public class BiomeDecoratorSynapse extends BiomeDecorator {
 
@@ -12,9 +12,9 @@ public class BiomeDecoratorSynapse extends BiomeDecorator {
 	{
 		if (this.chunk_X == 0 && this.chunk_Z == 0 && this.currentWorld.provider instanceof SynapseWorldProvider)
 		{
-			EntityDragon entitydragon = new EntityDragon(this.currentWorld);
-			entitydragon.setLocationAndAngles(0.0D, 64.0D, 0.0D, this.randomGenerator.nextFloat() * 360.0F, 0.0F);
-			this.currentWorld.spawnEntityInWorld(entitydragon);
+			EntityOverlordCore overlord = new EntityOverlordCore(this.currentWorld);
+			overlord.setLocationAndAngles(0.0D, 64.0D, 0.0D, this.randomGenerator.nextFloat() * 360.0F, 0.0F);
+			this.currentWorld.spawnEntityInWorld(overlord);
 		}
 	}
 }
