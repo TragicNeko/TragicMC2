2d1c15a1723f1145c433abfb958a62622ff19720
TragicNeko <TragicNeko@users.noreply.github.com>
2015-01-18 08:26:22 -0600

    Improve Dead Circuit worldgen, to reduce lag

--- src/main/java/tragicneko/tragicmc/worldgen/WorldGenDeadCircuit.java
+++ src/main/java/tragicneko/tragicmc/worldgen/WorldGenDeadCircuit.java
@@ -2,11 +2,13 @@ package tragicneko.tragicmc.worldgen;
 
 import static tragicneko.tragicmc.TragicBlocks.CircuitBlock;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 import net.minecraft.block.Block;
 import net.minecraft.world.World;
 import net.minecraft.world.gen.feature.WorldGenerator;
+import tragicneko.tragicmc.util.WorldHelper;
 
 public class WorldGenDeadCircuit extends WorldGenerator
 {
@@ -20,36 +22,13 @@ public class WorldGenDeadCircuit extends WorldGenerator
 	@Override
 	public boolean generate(World world, Random rand, int x, int y, int z)
 	{
-		while (world.isAirBlock(x, y, z) && y > 5)
-		{
-			--y;
-		}
+		ArrayList<int[]> list = WorldHelper.getBlocksInSphericalRange(world, this.blockSize - rand.nextInt(this.blockSize), x, y, z);
+		Block block;
 		
-		if (world.getBlock(x, y, z) != CircuitBlock) return false;
-
-		int l = rand.nextInt(this.blockSize - 2) + 2;
-		byte b0 = 1;
-
-		for (int i1 = x - l; i1 <= x + l; ++i1)
+		for (int[] coords : list)
 		{
-			for (int j1 = z - l; j1 <= z + l; ++j1)
-			{
-				int k1 = i1 - x;
-				int l1 = j1 - z;
-
-				if (k1 * k1 + l1 * l1 <= l * l)
-				{
-					for (int i2 = y - b0; i2 <= y + b0; ++i2)
-					{
-						Block block = world.getBlock(i1, i2, j1);
-
-						if (block == CircuitBlock)
-						{
-							world.setBlock(i1, i2, j1, CircuitBlock, rand.nextInt(4) + 1, 2);
-						}
-					}
-				}
-			}
+			block = world.getBlock(coords[0], coords[1], coords[2]);
+			if (block == CircuitBlock && rand.nextInt(5) == 0) world.setBlock(coords[0], coords[1], coords[2], CircuitBlock, rand.nextInt(4) + 1, 2);
 		}
 
 		return true;
