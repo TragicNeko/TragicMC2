ea3b1e9d54723a2e6949f64e4ca9a546d3e47ac7
TragicNeko <TragicNeko@users.noreply.github.com>
2015-03-08 10:23:00 -0500

    Add new entities to Synapse spawns

--- src/main/java/tragicneko/tragicmc/worldgen/biome/BiomeGenSynapse.java
+++ src/main/java/tragicneko/tragicmc/worldgen/biome/BiomeGenSynapse.java
@@ -1,20 +1,38 @@
 package tragicneko.tragicmc.worldgen.biome;
 
+import java.util.List;
+
+import net.minecraft.entity.EnumCreatureType;
 import net.minecraft.world.biome.BiomeGenBase;
 import tragicneko.tragicmc.TragicBlocks;
+import tragicneko.tragicmc.TragicConfig;
+import tragicneko.tragicmc.TragicMC;
+import tragicneko.tragicmc.entity.mob.EntityHarvester;
+import tragicneko.tragicmc.entity.mob.EntityHunter;
+import tragicneko.tragicmc.entity.mob.EntityLockbot;
 import tragicneko.tragicmc.entity.mob.EntityNanoSwarm;
 
 public class BiomeGenSynapse extends TragicBiome {
 
 	public BiomeGenSynapse(int par1) {
 		super(par1);
+		this.enableSnow = false;
+		this.enableRain = false;
 		this.spawnableMonsterList.clear();
         this.spawnableCreatureList.clear();
         this.spawnableWaterCreatureList.clear();
         this.spawnableCaveCreatureList.clear();
-        this.spawnableMonsterList.add(new BiomeGenBase.SpawnListEntry(EntityNanoSwarm.class, 5, 0, 1));
+        if (TragicConfig.allowNanoSwarm) this.spawnableMonsterList.add(new BiomeGenBase.SpawnListEntry(EntityNanoSwarm.class, TragicConfig.nanoSwarmSC, 0, 3));
+        if (TragicConfig.allowHunter) this.spawnableMonsterList.add(new BiomeGenBase.SpawnListEntry(EntityHunter.class, TragicConfig.hunterSC, 3, 6));
+        if (TragicConfig.allowLockbot) this.spawnableMonsterList.add(new BiomeGenBase.SpawnListEntry(EntityLockbot.class, TragicConfig.lockbotSC, 0, 1));
+        if (TragicConfig.allowHarvester) this.spawnableMonsterList.add(new BiomeGenBase.SpawnListEntry(EntityHarvester.class, TragicConfig.harvesterSC, 0, 1));
 		this.fillerBlock = TragicBlocks.CircuitBlock;
 		this.topBlock = TragicBlocks.CircuitBlock;
 		this.theBiomeDecorator = new BiomeDecoratorSynapse();
 	}
+	
+	public List getSpawnableList(EnumCreatureType p_76747_1_)
+    {
+        return super.getSpawnableList(p_76747_1_);
+    }
 }
