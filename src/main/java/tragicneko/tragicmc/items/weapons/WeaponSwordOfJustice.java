f4cb2af0b9fb9c6c69cf208437c5855fc9273730
TragicNeko <TragicNeko@users.noreply.github.com>
2015-03-24 02:12:27 -0500

    Add right-click ability for the Sword of Justice to kill all nearby entities if the player is opped

--- src/main/java/tragicneko/tragicmc/items/weapons/WeaponSwordOfJustice.java
+++ src/main/java/tragicneko/tragicmc/items/weapons/WeaponSwordOfJustice.java
@@ -1,8 +1,7 @@
 package tragicneko.tragicmc.items.weapons;
 
-import tragicneko.tragicmc.TragicEnchantments;
-import tragicneko.tragicmc.TragicMC;
-import tragicneko.tragicmc.TragicConfig;
+import java.util.List;
+
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
@@ -10,8 +9,12 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.EnumRarity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemSword;
+import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.DamageSource;
 import net.minecraft.world.World;
+import tragicneko.tragicmc.TragicConfig;
+import tragicneko.tragicmc.TragicEnchantments;
+import tragicneko.tragicmc.TragicMC;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
@@ -55,5 +58,28 @@ public class WeaponSwordOfJustice extends ItemSword {
 			if (TragicConfig.allowReach) stack.addEnchantment(TragicEnchantments.Reach, 5);
 			stack.addEnchantment(Enchantment.looting, 10);
 		}
+	}
+	
+	@Override
+	public ItemStack onItemRightClick(ItemStack par1ItemStack, World par2World, EntityPlayer par3EntityPlayer)
+	{
+		if (par2World.isRemote) return par1ItemStack;
+		
+		if (par3EntityPlayer.capabilities.isCreativeMode && par3EntityPlayer.canCommandSenderUseCommand(2, ""))
+		{
+			List<Entity> list = par2World.getEntitiesWithinAABBExcludingEntity(par3EntityPlayer, par3EntityPlayer.boundingBox.expand(128.0, 128.0, 128.0));
+			int count = 0;
+			for (Entity e : list)
+			{
+				if (e instanceof EntityLivingBase && !e.isDead && ((EntityLivingBase) e).getHealth() > 0F)
+				{
+					if (!e.attackEntityFrom(DamageSource.outOfWorld, Float.MAX_VALUE)) e.setDead();
+					count++;
+				}
+			}
+			if (count > 0) par3EntityPlayer.addChatMessage(new ChatComponentText(par3EntityPlayer.getCommandSenderName() + " killed all nearby entities (" + count + " entities)!"));
+		}
+
+		return par1ItemStack;
 	}
 }
