9d203b44866261b9128e0585e1a75498c3de7a90
TragicNeko <TragicNeko@users.noreply.github.com>
2015-04-08 19:36:15 -0500

    Added particles, made Aeris instance more difficult

--- src/main/java/tragicneko/tragicmc/blocks/BlockAeris.java
+++ src/main/java/tragicneko/tragicmc/blocks/BlockAeris.java
@@ -12,6 +12,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.IIcon;
 import net.minecraft.world.World;
+import tragicneko.tragicmc.TragicConfig;
 import tragicneko.tragicmc.TragicMC;
 import tragicneko.tragicmc.blocks.tileentity.TileEntityAeris;
 import cpw.mods.fml.relauncher.Side;
@@ -21,14 +22,15 @@ public class BlockAeris extends BlockTragicFlower implements ITileEntityProvider
 {
 	@SideOnly(Side.CLIENT)
 	private IIcon corruptingTexture, corruptedTexture;
-	
+
 	public BlockAeris()
 	{
 		super();
 		this.setBlockName("tragicmc.aeris");
 		this.setCreativeTab(TragicMC.Survival);
+		this.setTickRandomly(true);
 	}
-	
+
 	@Override
 	@SideOnly(Side.CLIENT)
 	public IIcon getIcon(int side, int meta)
@@ -45,38 +47,78 @@ public class BlockAeris extends BlockTragicFlower implements ITileEntityProvider
 		this.corruptedTexture = par1IconRegister.registerIcon("tragicmc:FullyCorruptedAeris");
 		this.corruptingTexture = par1IconRegister.registerIcon("tragicmc:PartiallyCorruptedAeris");
 	}
-	
+
 	@Override
 	public void getSubBlocks(Item par1, CreativeTabs par2, List par3)
 	{
 		for (int i = 0; i < 3; i++)
-		par3.add(new ItemStack(par1, 1, i));
+			par3.add(new ItemStack(par1, 1, i));
 	}
-	
+
 	@Override
 	public void func_149853_b(World p_149853_1_, Random p_149853_2_, int p_149853_3_, int p_149853_4_, int p_149853_5_) {}
-	
+
 	@Override
 	public TileEntity createNewTileEntity(World var1, int var2) {
 		return new TileEntityAeris();
 	}
-	
-	public void breakBlock(World p_149749_1_, int p_149749_2_, int p_149749_3_, int p_149749_4_, Block p_149749_5_, int p_149749_6_)
-    {
-        super.breakBlock(p_149749_1_, p_149749_2_, p_149749_3_, p_149749_4_, p_149749_5_, p_149749_6_);
-        p_149749_1_.removeTileEntity(p_149749_2_, p_149749_3_, p_149749_4_);
-    }
 
-    public boolean onBlockEventReceived(World p_149696_1_, int p_149696_2_, int p_149696_3_, int p_149696_4_, int p_149696_5_, int p_149696_6_)
-    {
-        super.onBlockEventReceived(p_149696_1_, p_149696_2_, p_149696_3_, p_149696_4_, p_149696_5_, p_149696_6_);
-        TileEntity tileentity = p_149696_1_.getTileEntity(p_149696_2_, p_149696_3_, p_149696_4_);
-        return tileentity != null ? tileentity.receiveClientEvent(p_149696_5_, p_149696_6_) : false;
-    }
+	public void breakBlock(World p_149749_1_, int p_149749_2_, int p_149749_3_, int p_149749_4_, Block p_149749_5_, int p_149749_6_)
+	{
+		super.breakBlock(p_149749_1_, p_149749_2_, p_149749_3_, p_149749_4_, p_149749_5_, p_149749_6_);
+		p_149749_1_.removeTileEntity(p_149749_2_, p_149749_3_, p_149749_4_);
+	}
+
+	public boolean onBlockEventReceived(World p_149696_1_, int p_149696_2_, int p_149696_3_, int p_149696_4_, int p_149696_5_, int p_149696_6_)
+	{
+		super.onBlockEventReceived(p_149696_1_, p_149696_2_, p_149696_3_, p_149696_4_, p_149696_5_, p_149696_6_);
+		TileEntity tileentity = p_149696_1_.getTileEntity(p_149696_2_, p_149696_3_, p_149696_4_);
+		return tileentity != null ? tileentity.receiveClientEvent(p_149696_5_, p_149696_6_) : false;
+	}
 
 	@Override
 	public int damageDropped(int par1)
 	{
 		return par1;
+	}
+
+	@SideOnly(Side.CLIENT)
+	public void randomDisplayTick(World world, int x, int y, int z, Random rand)
+	{
+		if (y <= 50 && world.provider.dimensionId == TragicConfig.dimensionID && !world.canBlockSeeTheSky(x, y, z)|| world.getBlockMetadata(x, y, z) >= 2)
+		{
+			int pow = world.getBlockMetadata(x, y, z) >= 2 ? 8 : 3;
+			
+			for (int l = 0; l < pow; ++l)
+			{
+				double d6 = (double)((float)x + rand.nextFloat());
+				double d1 = (double)((float)y + rand.nextFloat());
+				d6 = (double)((float)z + rand.nextFloat());
+				double d3 = 0.0D;
+				double d4 = 0.0D;
+				double d5 = 0.0D;
+				int i1 = rand.nextInt(2) * 2 - 1;
+				int j1 = rand.nextInt(2) * 2 - 1;
+				d3 = ((double)rand.nextFloat() - 0.5D) * 0.125D;
+				d4 = ((double)rand.nextFloat() - 0.5D) * 0.125D;
+				d5 = ((double)rand.nextFloat() - 0.5D) * 0.125D;
+				double d2 = (double)z + 0.5D + 0.25D * (double)j1;
+				d5 = (double)(rand.nextFloat() * 1.0F * (float)j1);
+				double d0 = (double)x + 0.5D + 0.25D * (double)i1;
+				d3 = (double)(rand.nextFloat() * 1.0F * (float)i1);
+				world.spawnParticle("portal", d0, d1, d2, d3, d4, d5);
+			}
+			
+			if (rand.nextInt(16) == 0 || world.getBlockMetadata(x, y, z) >= 2)
+			{				
+				for (int i = 0; i < 3; i++)
+				{
+					world.spawnParticle("witchMagic", x + (rand.nextDouble() - rand.nextDouble()) * 0.25 + 0.5,
+							y + rand.nextDouble() * 0.35 + 0.25,
+							z + (rand.nextDouble() - rand.nextDouble()) * 0.25 + 0.5,
+							0, 0, 0);
+				}
+			}
+		}
 	}
 }
--- src/main/java/tragicneko/tragicmc/blocks/tileentity/TileEntityAeris.java
+++ src/main/java/tragicneko/tragicmc/blocks/tileentity/TileEntityAeris.java
@@ -1,10 +1,19 @@
 package tragicneko.tragicmc.blocks.tileentity;
 
-import tragicneko.tragicmc.TragicMC;
+import java.util.ArrayList;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.ChatComponentText;
+import tragicneko.tragicmc.TragicBlocks;
+import tragicneko.tragicmc.TragicMC;
+import tragicneko.tragicmc.items.ItemCorruptedEgg;
+import tragicneko.tragicmc.util.WorldHelper;
 
 public class TileEntityAeris extends TileEntity {
-	
+
 	private int corruptedTicks;
 
 	@Override
@@ -14,13 +23,47 @@ public class TileEntityAeris extends TileEntity {
 		if (!this.worldObj.isRemote && this.yCoord <= 50 && !this.worldObj.canBlockSeeTheSky(this.xCoord, this.yCoord, this.zCoord) && this.blockMetadata < 2)
 		{
 			corruptedTicks++;
-			
+			this.worldObj.getChunkFromBlockCoords(this.xCoord, this.zCoord).inhabitedTime += 100;
+			EntityPlayer player = this.worldObj.getClosestPlayer(this.xCoord, this.yCoord, this.zCoord, 16.0D); 
+
+			if (this.corruptedTicks % 300 == 0 && player != null && this.corruptedTicks < 3000)
+			{
+				player.addChatMessage(new ChatComponentText("Aeris is corrupting... (" + (this.corruptedTicks / 300)  + " /10)"));
+				TragicMC.logInfo("Aeris metadata is " + this.blockMetadata);
+
+				if (this.blockMetadata == 1)
+				{
+					for (int i = 0; i < this.worldObj.rand.nextInt(3) + 1; i++)
+					{
+						TragicMC.logInfo("Aeris attempted to spawn in an entity based on biome.");
+						ItemCorruptedEgg.spawnCreature(this.worldObj, this.xCoord + (this.worldObj.rand.nextDouble() - this.worldObj.rand.nextDouble() * 4),
+								this.yCoord,
+								this.zCoord + (this.worldObj.rand.nextDouble() - this.worldObj.rand.nextDouble() * 4));
+					}
+				}
+			}
+
 			if (this.corruptedTicks >= 3000)
 			{
 				this.corruptedTicks = 0;
 				this.worldObj.setBlockMetadataWithNotify(this.xCoord, this.yCoord, this.zCoord, blockMetadata + 1, 2);
 				TragicMC.logInfo("Aeris block had it's meta updated to " + this.blockMetadata + 1);
-				this.blockMetadata += 1;
+				String s = this.blockMetadata == 1 ? "Aeris is starting to show signs of corruption..." : "Aeris has fully corrupted!";
+				if (player != null) player.addChatMessage(new ChatComponentText(s));
+			}
+
+			if (this.corruptedTicks % 20 == 0)
+			{
+				ArrayList<int[]> list = WorldHelper.getBlocksInSphericalRange(this.worldObj, 5.0, this.xCoord, this.yCoord, this.zCoord);
+				Block block;
+				for (int[] coords : list)
+				{
+					block = this.worldObj.getBlock(coords[0], coords[1], coords[2]);
+					if (block != TragicBlocks.Aeris && coords[1] >= this.yCoord && block.getMaterial() != Material.air)
+					{
+						this.worldObj.func_147480_a(coords[0], coords[1], coords[2], true);
+					}
+				}
 			}
 		}
 	}
