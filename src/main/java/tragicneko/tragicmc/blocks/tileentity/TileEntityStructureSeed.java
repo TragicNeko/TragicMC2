051d189a009a15de8cd591c33a248895054081e3
TragicNeko <TragicNeko@users.noreply.github.com>
2015-04-14 23:57:12 -0500

    Only allow Spire to be generated in the Collision

--- src/main/java/tragicneko/tragicmc/blocks/tileentity/TileEntityStructureSeed.java
+++ src/main/java/tragicneko/tragicmc/blocks/tileentity/TileEntityStructureSeed.java
@@ -3,8 +3,10 @@ package tragicneko.tragicmc.blocks.tileentity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ChatComponentText;
+import tragicneko.tragicmc.TragicConfig;
 import tragicneko.tragicmc.TragicMC;
 import tragicneko.tragicmc.worldgen.structure.Structure;
+import tragicneko.tragicmc.worldgen.structure.StructureCorruptedSpire;
 
 public class TileEntityStructureSeed extends TileEntity {
 
@@ -20,6 +22,11 @@ public class TileEntityStructureSeed extends TileEntity {
 
 		Structure structure = Structure.structureList[meta];
 		if (structure == null) return;
+		if (structure instanceof StructureCorruptedSpire && this.worldObj.provider.dimensionId != TragicConfig.dimensionID)
+		{
+			EntityPlayer player = this.worldObj.getClosestPlayer(this.xCoord, this.yCoord, this.zCoord, 16.0);
+			if (player != null) player.addChatMessage(new ChatComponentText(structure.structureName + " must be generated in the Collision!"));
+		}
 		if (structure.generateStructureWithVariant(this.worldObj.rand.nextInt(structure.getVariantSize()), this.worldObj, this.worldObj.rand, this.xCoord, this.yCoord, this.zCoord))
 		{
 			EntityPlayer player = this.worldObj.getClosestPlayer(this.xCoord, this.yCoord, this.zCoord, 16.0);
