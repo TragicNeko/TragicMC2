9d8510e20899a1dfc6c6174511ff81a1c1300f37
TragicNeko <TragicNeko@users.noreply.github.com>
2015-04-19 01:48:30 -0500

    Fixed height value for Obsidian Cavern, made Soul tombs more bulky

--- src/main/java/tragicneko/tragicmc/worldgen/schematic/SchematicSoulTomb.java
+++ src/main/java/tragicneko/tragicmc/worldgen/schematic/SchematicSoulTomb.java
@@ -9,7 +9,7 @@ import tragicneko.tragicmc.TragicBlocks;
 import tragicneko.tragicmc.TragicItems;
 
 public class SchematicSoulTomb extends Schematic {
-	
+
 	public static Block[] blocks = new Block[] {TragicBlocks.DarkCobblestone, TragicBlocks.DarkenedQuartz, TragicBlocks.SmoothNetherrack,
 		Blocks.stonebrick, TragicBlocks.ScorchedRock, TragicBlocks.ErodedStone};
 
@@ -30,7 +30,7 @@ public class SchematicSoulTomb extends Schematic {
 				}
 			}
 		}
-		
+
 		Block block = blocks[variant];
 
 		for (int x1 = -3; x1 < 4; x1++)
@@ -40,19 +40,37 @@ public class SchematicSoulTomb extends Schematic {
 				if (Math.abs(x1) != 3 && Math.abs(z1) != 3 && Math.abs(x1) != Math.abs(z1)) world.setBlock(x + x1, y, z + z1, block, 1, 2);
 			}
 		}
-		
+
 		world.setBlock(x, y, z, block, 1, 2);
 		world.setBlock(x + 1, y, z + 1, Blocks.lava);
 		world.setBlock(x + 1, y, z - 1, Blocks.lava);
 		world.setBlock(x - 1, y, z + 1, Blocks.lava);
 		world.setBlock(x - 1, y, z - 1, Blocks.lava);
-		
+
 		world.setBlock(x + 3, y + 1, z, block, 1, 2);
 		world.setBlock(x - 3, y + 1, z, block, 1, 2);
 		world.setBlock(x, y + 1, z + 3, block, 1, 2);
 		world.setBlock(x, y + 1, z - 3, block, 1, 2);
+		
+		world.setBlock(x + 3, y + 1, z + 1, block, 1, 2);
+		world.setBlock(x - 3, y + 1, z + 1, block, 1, 2);
+		world.setBlock(x + 1, y + 1, z + 3, block, 1, 2);
+		world.setBlock(x + 1, y + 1, z - 3, block, 1, 2);
+		
+		world.setBlock(x + 3, y + 1, z - 1, block, 1, 2);
+		world.setBlock(x - 3, y + 1, z - 1, block, 1, 2);
+		world.setBlock(x - 1, y + 1, z + 3, block, 1, 2);
+		world.setBlock(x - 1, y + 1, z - 3, block, 1, 2);
 
-		for (int y1 = 2; y1 < 6; y1++)
+		for (int y1 = 1; y1 < 8; y1++)
+		{
+			world.setBlock(x + 2, y + y1, z + 2, block, 1, 2);
+			world.setBlock(x + 2, y + y1, z - 2, block, 1, 2);
+			world.setBlock(x - 2, y + y1, z + 2, block, 1, 2);
+			world.setBlock(x - 2, y + y1, z - 2, block, 1, 2);
+		}
+
+		for (int y1 = 1; y1 < 8; y1++)
 		{
 			world.setBlock(x + 4, y + y1, z, block, 1, 2);
 			world.setBlock(x - 4, y + y1, z, block, 1, 2);
@@ -67,7 +85,7 @@ public class SchematicSoulTomb extends Schematic {
 			world.setBlock(x, y + y1, z + 3, block, 1, 2);
 			world.setBlock(x, y + y1, z - 3, block, 1, 2);
 		}
-		
+
 		for (int x1 = -3; x1 < 4; x1++)
 		{
 			world.setBlock(x + x1, y + 6, z + 3, block, 1, 2);
@@ -75,7 +93,7 @@ public class SchematicSoulTomb extends Schematic {
 			world.setBlock(x + 3, y + 6, z + x1, block, 1, 2);
 			world.setBlock(x - 3, y + 6, z + x1, block, 1, 2);
 		}
-		
+
 		for (int x1 = -2; x1 < 3; x1++)
 		{
 			for (int z1 = -2; z1 < 3; z1++)
@@ -83,26 +101,30 @@ public class SchematicSoulTomb extends Schematic {
 				if (Math.abs(x1) != 3 && Math.abs(z1) != 3 && Math.abs(x1) != Math.abs(z1)) world.setBlock(x + x1, y + 7, z + z1, block, 1, 2);
 			}
 		}
-		
+
 		world.setBlock(x, y + 7, z, block, 1, 2);
-		
+
 		world.setBlock(x + 2, y + 8, z, block, 1, 2);
 		world.setBlock(x - 2, y + 8, z, block, 1, 2);
 		world.setBlock(x, y + 8, z + 2, block, 1, 2);
 		world.setBlock(x, y + 8, z - 2, block, 1, 2);
-		
+
 		world.setBlock(x + 1, y + 9, z, block, 1, 2);
 		world.setBlock(x - 1, y + 9, z, block, 1, 2);
 		world.setBlock(x, y + 9, z + 1, block, 1, 2);
 		world.setBlock(x, y + 9, z - 1, block, 1, 2);
-		
+
 		world.setBlock(x, y + 9, z, block, 1, 2);
 		world.setBlock(x, y + 10, z, block, 1, 2);
-		
+
 		world.setBlock(x, y + 1, z, TragicBlocks.SoulChest, 0, 2);
 		this.applyChestContents(world, rand, x, y + 1, z, TragicItems.NetherStructureHook);
-		
-		world.setBlock(x, y + 8, z, Blocks.flowing_lava, 0, 2);
+
+		world.setBlock(x, y + 8, z, block, 1, 2);
+		world.setBlock(x, y + 8, z + 1, Blocks.flowing_lava, 0, 2);
+		world.setBlock(x, y + 8, z - 1, Blocks.flowing_lava, 0, 2);
+		world.setBlock(x + 1, y + 8, z, Blocks.flowing_lava, 0, 2);
+		world.setBlock(x - 1, y + 8, z, Blocks.flowing_lava, 0, 2);
 
 		return true;
 	}
