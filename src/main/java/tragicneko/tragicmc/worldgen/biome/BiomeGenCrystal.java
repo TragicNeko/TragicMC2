8a9928aaf1b640b7f71ee467f6d19fc88b7ee7aa
TragicNeko <TragicNeko@users.noreply.github.com>
2015-04-19 23:38:41 -0500

    Added Ranmas to Crystal biome

--- src/main/java/tragicneko/tragicmc/worldgen/biome/BiomeGenCrystal.java
+++ src/main/java/tragicneko/tragicmc/worldgen/biome/BiomeGenCrystal.java
@@ -1,5 +1,9 @@
 package tragicneko.tragicmc.worldgen.biome;
 
+import net.minecraft.world.biome.BiomeGenBase;
+import tragicneko.tragicmc.TragicConfig;
+import tragicneko.tragicmc.entity.mob.EntityRanmas;
+
 public class BiomeGenCrystal extends TragicBiome {
 
 	public BiomeGenCrystal(int par1) {
@@ -8,6 +12,7 @@ public class BiomeGenCrystal extends TragicBiome {
 		this.spawnableMonsterList.clear();
 		this.spawnableCaveCreatureList.clear();
 		this.spawnableWaterCreatureList.clear();
+		this.spawnableMonsterList.add(new BiomeGenBase.SpawnListEntry(EntityRanmas.class, TragicConfig.ranmasSC, 0, 1));
 		this.enableRain = false;
 		this.enableSnow = false;
 		this.rainfall = 0F;
