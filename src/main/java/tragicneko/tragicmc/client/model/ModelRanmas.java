9bec1fcd069d91624c4e00a2d80e0e0619a3a632
TragicNeko <TragicNeko@users.noreply.github.com>
2015-04-19 23:36:15 -0500

    Added Ranmas along with it's model/texture

---
+++ src/main/java/tragicneko/tragicmc/client/model/ModelRanmas.java
@@ -0,0 +1,75 @@
+package tragicneko.tragicmc.client.model;
+
+import net.minecraft.client.model.ModelBase;
+import net.minecraft.client.model.ModelRenderer;
+import net.minecraft.entity.Entity;
+
+/**
+ * ModelRanmas - TragicNeko
+ * Created using Tabula 4.1.1
+ */
+public class ModelRanmas extends ModelBase {
+    public ModelRenderer head;
+    public ModelRenderer poleFR;
+    public ModelRenderer poleFL;
+    public ModelRenderer poleBL;
+    public ModelRenderer poleBR;
+    public ModelRenderer top;
+    public ModelRenderer bottom;
+    public ModelRenderer top2;
+    public ModelRenderer shape9;
+
+    public ModelRanmas() {
+        this.textureWidth = 64;
+        this.textureHeight = 64;
+        this.poleBL = new ModelRenderer(this, 32, 32);
+        this.poleBL.setRotationPoint(0.0F, 0.0F, 0.0F);
+        this.poleBL.addBox(4.0F, -10.0F, 4.0F, 2, 16, 2, 0.0F);
+        this.head = new ModelRenderer(this, 0, 32);
+        this.head.setRotationPoint(0.0F, 9.0F, 0.0F);
+        this.head.addBox(-3.0F, -5.0F, -3.0F, 6, 8, 6, 0.0F);
+        this.bottom = new ModelRenderer(this, 0, 0);
+        this.bottom.setRotationPoint(0.0F, 0.0F, 0.0F);
+        this.bottom.addBox(-8.0F, 6.0F, -8.0F, 16, 5, 16, 0.0F);
+        this.poleFL = new ModelRenderer(this, 32, 32);
+        this.poleFL.setRotationPoint(0.0F, 0.0F, 0.0F);
+        this.poleFL.addBox(4.0F, -10.0F, -6.0F, 2, 16, 2, 0.0F);
+        this.top2 = new ModelRenderer(this, 0, 0);
+        this.top2.setRotationPoint(0.0F, 0.0F, 0.0F);
+        this.top2.addBox(-3.0F, -16.0F, -3.0F, 6, 4, 6, 0.0F);
+        this.poleFR = new ModelRenderer(this, 32, 32);
+        this.poleFR.setRotationPoint(0.0F, 0.0F, 0.0F);
+        this.poleFR.addBox(-6.0F, -10.0F, -6.0F, 2, 16, 2, 0.0F);
+        this.poleBR = new ModelRenderer(this, 32, 32);
+        this.poleBR.setRotationPoint(0.0F, 0.0F, 0.0F);
+        this.poleBR.addBox(-6.0F, -10.0F, 4.0F, 2, 16, 2, 0.0F);
+        this.top = new ModelRenderer(this, 0, 0);
+        this.top.setRotationPoint(0.0F, 0.0F, 0.0F);
+        this.top.addBox(-7.0F, -12.0F, -7.0F, 14, 4, 14, 0.0F);
+        this.shape9 = new ModelRenderer(this, 0, 0);
+        this.shape9.setRotationPoint(0.0F, 0.0F, 0.0F);
+        this.shape9.addBox(-3.0F, 11.0F, -3.0F, 6, 3, 6, 0.0F);
+        this.head.addChild(this.poleBL);
+        this.head.addChild(this.bottom);
+        this.head.addChild(this.poleFL);
+        this.head.addChild(this.top2);
+        this.head.addChild(this.poleFR);
+        this.head.addChild(this.poleBR);
+        this.head.addChild(this.top);
+        this.head.addChild(this.shape9);
+    }
+
+    @Override
+    public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5) { 
+        this.head.render(f5);
+    }
+
+    /**
+     * This is a helper function from Tabula to set the rotation of model parts
+     */
+    public void setRotateAngle(ModelRenderer modelRenderer, float x, float y, float z) {
+        modelRenderer.rotateAngleX = x;
+        modelRenderer.rotateAngleY = y;
+        modelRenderer.rotateAngleZ = z;
+    }
+}
---
+++ src/main/java/tragicneko/tragicmc/entity/mob/EntityRanmas.java
@@ -0,0 +1,202 @@
+package tragicneko.tragicmc.entity.mob;
+
+import static tragicneko.tragicmc.TragicConfig.ranmasStats;
+import net.minecraft.command.IEntitySelector;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.EnumCreatureAttribute;
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.ai.EntityAIAttackOnCollide;
+import net.minecraft.entity.ai.EntityAIHurtByTarget;
+import net.minecraft.entity.ai.EntityAINearestAttackableTarget;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.AxisAlignedBB;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.MathHelper;
+import net.minecraft.world.World;
+import tragicneko.tragicmc.TragicEntities;
+import tragicneko.tragicmc.TragicItems;
+import tragicneko.tragicmc.TragicMC;
+import tragicneko.tragicmc.entity.EntityAIWatchTarget;
+
+public class EntityRanmas extends TragicMob {
+
+	private double[] motions = new double[] {0, 0, 0};
+	private int chargeTicks = 0;
+	private int chargeBuffer = 120;
+
+	public EntityRanmas(World par1World) {
+		super(par1World);
+		this.setSize(1.355F, 2.425F);
+		this.isImmuneToFire = true;
+		this.tasks.addTask(0, new EntityAIAttackOnCollide(this, EntityLivingBase.class, 1.0D, true));
+		this.tasks.addTask(4, new EntityAIWatchTarget(this, 64.0F));
+		this.targetTasks.addTask(2, new EntityAIHurtByTarget(this, true));
+		this.targetTasks.addTask(3, new EntityAINearestAttackableTarget(this, EntityLivingBase.class, 0, true, false));
+	}
+
+	@Override
+	protected boolean isChangeAllowed() {
+		return false;
+	}
+
+	@Override
+	public boolean isAIEnabled()
+	{
+		return true;
+	}
+
+	@Override
+	protected void applyEntityAttributes()
+	{
+		super.applyEntityAttributes();
+		this.getEntityAttribute(SharedMonsterAttributes.maxHealth).setBaseValue(ranmasStats[0]);
+		this.getEntityAttribute(SharedMonsterAttributes.movementSpeed).setBaseValue(ranmasStats[1]);
+		this.getEntityAttribute(SharedMonsterAttributes.attackDamage).setBaseValue(ranmasStats[2]);
+		this.getEntityAttribute(SharedMonsterAttributes.followRange).setBaseValue(ranmasStats[3]);
+		this.getEntityAttribute(SharedMonsterAttributes.knockbackResistance).setBaseValue(ranmasStats[4]);
+	}
+
+	@Override
+	public int getTotalArmorValue()
+	{
+		return (int) ranmasStats[5];
+	}
+
+	@Override
+	public EnumCreatureAttribute getCreatureAttribute()
+	{
+		return TragicEntities.Natural;
+	}
+
+	@Override
+	protected boolean canCorrupt()
+	{
+		return false;
+	}
+
+	@Override
+	public boolean canRenderOnFire()
+	{
+		return false;
+	}
+
+	@Override
+	public void onLivingUpdate()
+	{
+		if (this.worldObj.isRemote || this.chargeTicks == 0) this.motionX = this.motionY = this.motionZ = 0;
+		super.onLivingUpdate();
+
+		if (this.worldObj.isRemote) return;
+
+		if (this.chargeBuffer > 0) --this.chargeBuffer;
+
+		if (this.chargeTicks > 0)
+		{
+			this.chargeTicks--;
+			this.chargeBuffer = 40 + (int) (60 * (this.getHealth() / this.getMaxHealth()));
+
+			if (this.isCollidedHorizontally)
+			{
+				TragicMC.logInfo("Ranmas was collided");
+				this.chargeTicks = 0;
+
+				double x = this.motions[0];
+				double y = this.motions[1];
+				double z = this.motions[2];
+				this.motions = new double[] {0, 0, 0};
+
+				float f = MathHelper.sqrt_double(x * x + z * z + y * y) * 5.0F;
+				TragicMC.logInfo("Collision value was " + f);
+				if (f >= 2.0F)
+				{
+					if (f > 5.0F) f = 5.0F;
+					this.worldObj.createExplosion(this, this.posX, this.posY, this.posZ, (f / 2) * rand.nextFloat() + (f / 2), this.getMobGriefing());
+					this.attackEntityFrom(DamageSource.fall, f);
+				}
+			}
+			else
+			{
+				this.motionX = this.motions[0];
+				this.motionY = this.motions[1];
+				this.motionZ = this.motions[2];
+				this.motions = new double[] {this.motionX, this.motionY, this.motionZ};
+			}
+		}
+		else
+		{
+			if (this.getAttackTarget() != null && this.canEntityBeSeen(this.getAttackTarget()) && this.ticksExisted % 5 == 0 && rand.nextInt(8) == 0 && this.chargeBuffer == 0)
+			{
+				double d0 = MathHelper.clamp_double(this.getAttackTarget().posX - this.posX, -2.4D, 2.4D);
+				double d1 = MathHelper.clamp_double(this.getAttackTarget().boundingBox.minY + this.getAttackTarget().height / 2.0F - (this.posY + this.height / 2.0F), -2.4D, 2.4D);
+				double d2 = MathHelper.clamp_double(this.getAttackTarget().posZ - this.posZ, -2.4D, 2.4D);
+				double d3 = (double) MathHelper.sqrt_double(d0 * d0 + d1 * d1 + d2 * d2);
+
+				this.motions = new double[] {d0, d1, d2};
+				this.chargeTicks = 30;
+			}
+			else if (this.getAttackTarget() == null && this.ticksExisted % 20 == 0)
+			{
+				double d4 = this.getEntityAttribute(SharedMonsterAttributes.followRange).getAttributeValue();
+				this.setAttackTarget((EntityLivingBase) this.worldObj.findNearestEntityWithinAABB(EntityLivingBase.class, this.boundingBox.expand(d4, d4, d4), this));
+			}
+		}
+		/*
+		if (this.ticksExisted % 5 == 0)
+		{
+			TragicMC.logInfo("Charge ticks: " + this.chargeTicks);
+			TragicMC.logInfo("Charge buffer: " + this.chargeBuffer);
+			TragicMC.logInfo("Motions: " + this.motions[0] + ", " + this.motions[1] + ", " + this.motions[2]);
+			TragicMC.logInfo("Has target? " + (this.getAttackTarget() != null));
+		} */
+	}
+
+	@Override
+	public boolean attackEntityFrom(DamageSource src, float dmg)
+	{
+		boolean flag = false;
+
+		if (!this.worldObj.isRemote && src.getEntity() != null && src.getEntity() instanceof EntityPlayer)
+		{
+			EntityPlayer player = (EntityPlayer) src.getEntity();
+			flag = player.getCurrentEquippedItem() != null && (player.getCurrentEquippedItem().getItem() == TragicItems.SwordOfJustice || player.getCurrentEquippedItem().getItem() == TragicItems.BowOfJustice);
+		}
+
+		if (flag || !flag && src.getEntity() != null && src.getEntity() instanceof EntityRanmas || src.canHarmInCreative() || src == DamageSource.fall)
+		{
+			TragicMC.logInfo("Damage was " + dmg);
+			return super.attackEntityFrom(src, dmg);
+		}
+
+		return !this.worldObj.isRemote;
+	}
+
+	@Override
+	public boolean attackEntityAsMob(Entity par1Entity)
+	{
+		if (this.worldObj.isRemote) return false;
+
+		double x = this.motions[0];
+		double y = this.motions[1];
+		double z = this.motions[2];
+
+		float f = MathHelper.sqrt_double(x * x + z * z + y * y) * 5.0F;
+		if (f < 1.0F) f = 1.0F;
+		if (f > 20.0F) f = 20.0F;
+		boolean flag = par1Entity.attackEntityFrom(DamageSource.causeMobDamage(this), f * (float) this.getEntityAttribute(SharedMonsterAttributes.attackDamage).getAttributeValue());
+		TragicMC.logInfo("Damage value was " + f);
+		
+		if (flag && this.chargeTicks > 0)
+		{
+			par1Entity.motionX *= 2.25D;
+			par1Entity.motionZ *= 2.25D;
+		}
+		return flag;
+	}
+
+	@Override
+	public void fall(float par1) {}
+
+	@Override
+	public void updateFallState(double par1, boolean par2) {}
+}
