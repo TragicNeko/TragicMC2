e27581055c6c133c2404f98e31236f85e74ea90f
TragicNeko <TragicNeko@users.noreply.github.com>
2015-04-19 23:40:06 -0500

    Allow biome sizes to be configurable

--- src/main/java/tragicneko/tragicmc/dimension/TragicGenLayer.java
+++ src/main/java/tragicneko/tragicmc/dimension/TragicGenLayer.java
@@ -1,18 +1,17 @@
 package tragicneko.tragicmc.dimension;
 
+import tragicneko.tragicmc.TragicConfig;
 import net.minecraft.world.WorldType;
 import net.minecraft.world.gen.layer.GenLayer;
 import net.minecraft.world.gen.layer.GenLayerVoronoiZoom;
 import net.minecraft.world.gen.layer.GenLayerZoom;
 
 public class TragicGenLayer {
-	
-	private static final int biomeSize = 6;
 
 	public GenLayer[] createWorld(long seed, WorldType worldType)
 	{
 		GenLayer biomes = new TragicBiomeGenLayer(seed);
-		biomes = GenLayerZoom.magnify(seed, biomes, TragicGenLayer.biomeSize);
+		biomes = GenLayerZoom.magnify(seed, biomes, TragicConfig.collisionBiomeSize);
 		GenLayerVoronoiZoom voronoiZoom = new GenLayerVoronoiZoom(seed, biomes);
 		
 		biomes.initWorldGenSeed(seed);
