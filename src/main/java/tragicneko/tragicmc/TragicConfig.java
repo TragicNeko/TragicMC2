a0b8fb7bd53e23956f54c599594b24015495a1d2
TragicNeko <TragicNeko@users.noreply.github.com>
2015-04-21 03:19:44 -0500

    Slight reduction of health from previous change

--- src/main/java/tragicneko/tragicmc/TragicConfig.java
+++ src/main/java/tragicneko/tragicmc/TragicConfig.java
@@ -791,6 +791,7 @@ public class TragicConfig {
 		mapping = 0;
 		bossStats[mapping++] = (config.get(catMobs, "apisStats", new double[] {160.0, 0.325, 8.0, 32.0, 1.0, 15}).getDoubleList());
 		bossStats[mapping++] = (config.get(catMobs, "deathReaperStats", new double[] {220.0, 0.350, 16.0, 32.0, 1.0, 20}).getDoubleList());
+		bossStats[mapping++] = (config.get(catMobs, "kitsuneStats", new double[] {80.0, 0.420, 6.0, 64.0, 0.0, 0}).getDoubleList());
 		bossStats[mapping++] = (config.get(catMobs, "polarisStats", new double[] {120.0, 0.390, 5.0, 64.0, 0.0, 14}).getDoubleList());
 		bossStats[mapping++] = (config.get(catMobs, "yetiStats", new double[] {140.0, 0.326, 12.0, 48.0, 2.0, 22}).getDoubleList());
 		bossStats[mapping++] = (config.get(catMobs, "timeControllerStats", new double[] {350.0, 0.386, 6.0, 64.0, 0.5, 16}).getDoubleList());
