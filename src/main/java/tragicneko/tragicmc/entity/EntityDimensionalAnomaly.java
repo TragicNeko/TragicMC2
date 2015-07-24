3d974471feff851cd846679b562e5c52753d0459
TragicNeko <TragicNeko@users.noreply.github.com>
2015-07-24 03:34:41 -0500

    Improved ability to locate and utilize Dimensional Anomalies

--- src/main/java/tragicneko/tragicmc/entity/EntityDimensionalAnomaly.java
+++ src/main/java/tragicneko/tragicmc/entity/EntityDimensionalAnomaly.java
@@ -18,8 +18,8 @@ public class EntityDimensionalAnomaly extends Entity {
 
 	public EntityDimensionalAnomaly(World par1World) {
 		super(par1World);
-		this.setSize(0.675F, 0.825F);
-		if (!par1World.isRemote) this.setTimeToLive(150 + rand.nextInt(100));
+		this.setSize(0.675F, 1.225F);
+		if (!par1World.isRemote) this.setTimeToLive(210 + rand.nextInt(100));
 		this.preventEntitySpawning = true;
 		this.isImmuneToFire = true;
 	}
@@ -49,44 +49,30 @@ public class EntityDimensionalAnomaly extends Entity {
 
 		if (this.worldObj.isRemote)
 		{
-			double d7 = rand.nextDouble() - rand.nextDouble();
-			double d8 = rand.nextDouble() - rand.nextDouble();
-			double d9 = rand.nextDouble() - rand.nextDouble();
-
-			double d0 = d7 + this.posX;
-			double d1 = d8 + this.posY + this.height / 2.0D;
-			double d2 = d9 + this.posZ;
-
-			float f2 = MathHelper.sqrt_double(d0 * d0 + d1 * d1 + d2 * d2);
-			double d3 = 0.5D;
-
-			double d4 = d0 / f2 * d3 * 0.200000011920929D + d7 * 0.20000000298023224D;
-			double d5 = d1 / f2 * d3 * 0.200000011920929D + d8 * 0.20000000298023224D;
-			double d6 = d2 / f2 * d3 * 0.200000011920929D + d9 * 0.20000000298023224D;
-
 			for (int i = 0; i < 8; i++)
 			{
-				this.worldObj.spawnParticle("reddust", d0, d1, d2, rand.nextFloat() * 2.25F, rand.nextFloat() * 2.25F, rand.nextFloat() * 2.25F);
+				this.worldObj.spawnParticle("reddust", this.posX + rand.nextDouble() - rand.nextDouble(), this.posY + rand.nextDouble() * this.height,
+						this.posZ + rand.nextDouble() - rand.nextDouble(), rand.nextFloat() * 2.25F, rand.nextFloat() * 2.25F, rand.nextFloat() * 2.25F);
 			}
 
-			if (rand.nextInt(16) == 0)
-			{
-				for (int i = 0; i < 4; i++)
-				{
-					this.worldObj.spawnParticle("cloud", d0, d1, d2, d4 * -0.5, d5 * -0.5, d6 * -0.5);
-				}
-			}
-
-			if (this.ticksExisted >= this.getTimeToLive()) this.worldObj.spawnParticle("cloud", this.posX, this.posY, this.posZ, 0D, 0D, 0D);
+			if (this.ticksExisted >= this.getTimeToLive()) for (byte b = 0; b < 32; b++) this.worldObj.spawnParticle("smoke", this.posX + rand.nextDouble() - rand.nextDouble(), this.posY + rand.nextDouble() * this.height,
+					this.posZ + rand.nextDouble() - rand.nextDouble(), rand.nextDouble() - rand.nextDouble(), rand.nextDouble() - rand.nextDouble(), rand.nextDouble() - rand.nextDouble());
 			return;
 		}
 
-		if (this.ticksExisted >= this.getTimeToLive()) this.setDead();
+		if (this.ticksExisted >= this.getTimeToLive())
+		{
+			this.setDead();
+			this.playSound("tragicmc:random.truncatedbeep", 0.2F, 0.8F);
+		}
+
 		if (!this.onGround)
 		{
 			this.motionY = -0.035D;
 			this.moveEntity(0.0, -0.035, 0.0);
 		}
+
+		if (this.ticksExisted % 10 == 0 && this.ticksExisted + 10 < this.getTimeToLive()) this.worldObj.playSoundAtEntity(this, "tragicmc:random.truncatedbeep", 0.2F, 1.8F);
 
 		if (TragicConfig.allowDivinity && this.ticksExisted % 5 == 0)
 		{
