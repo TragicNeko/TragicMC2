aee0cc09f06e6b0d5dfed97eaefeba446509ea3f
TragicNeko <TragicNeko@users.noreply.github.com>
2015-12-14 05:57:21 -0600

    Display Aegar's health since it's a Boss

--- src/main/java/tragicneko/tragicmc/client/render/boss/RenderAegar.java
+++ src/main/java/tragicneko/tragicmc/client/render/boss/RenderAegar.java
@@ -1,31 +1,17 @@
 package tragicneko.tragicmc.client.render.boss;
 
-import net.minecraft.client.renderer.entity.RenderLiving;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.util.ResourceLocation;
-
-import org.lwjgl.opengl.GL11;
-
 import tragicneko.tragicmc.client.model.ModelAegar;
 import tragicneko.tragicmc.entity.boss.EntityAegar;
 
-public class RenderAegar extends RenderLiving {
+public class RenderAegar extends RenderBoss {
 
 	private static final ResourceLocation texture = new ResourceLocation("tragicmc:textures/mobs/Aegar.png");
 	private static final ResourceLocation texture2 = new ResourceLocation("tragicmc:textures/mobs/AegarDamaged.png");
 
-	protected float scale;
-
 	public RenderAegar() {
-		super(new ModelAegar(), 0.845F);
-		this.scale = 1.745F;
-	}
-
-	@Override
-	protected void preRenderCallback(EntityLivingBase entity, float par2)
-	{
-		GL11.glScalef(scale, scale, scale);
+		super(new ModelAegar(), 0.845F, 1.745F);
 	}
 
 	@Override
